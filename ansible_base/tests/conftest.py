7fb1d6cd7f0b7b635e05d1c51e0193072973eac2
Rick Elrod <rick@elrod.me>
2023-11-08 12:24:32 -0600

    Ensure test fixture copies land in the right module
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- ansible_base/tests/conftest.py
+++ ansible_base/tests/conftest.py
@@ -14,9 +14,13 @@ def copy_fixture(copies=1):
     def wrapper(func):
         if '_pytestfixturefunction' not in dir(func):
             raise TypeError(f"Can't apply copy_fixture to {func.__name__} because it is not a fixture. HINT: @copy_fixture must be *above* @pytest.fixture")
+
+        module_name = func.__module__
+        module = __import__(module_name, fromlist=[''])
+
         for i in range(copies):
             new_name = f"{func.__name__}_{i + 1}"
-            globals()[new_name] = func
+            setattr(module, new_name, func)
         return func
 
     return wrapper
