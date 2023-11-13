514e25e9f83f43d8057fe0f3131905a3433dfaad
John Westcott IV <john.westcott.iv@redhat.com>
2023-11-13 13:47:28 -0500

    Move encryption/decryption to CommonModel

--- ansible_base/serializers/common.py
+++ ansible_base/serializers/common.py
@@ -3,6 +3,8 @@ import logging
 from rest_framework import serializers
 from rest_framework.reverse import reverse_lazy
 
+from ansible_base.utils.encryption import ENCRYPTED_STRING
+
 logger = logging.getLogger('ansible_base.serializers')
 
 
@@ -35,6 +37,24 @@ class CommonModelSerializer(serializers.ModelSerializer):
             logger.warning(f"Object {obj.__class__} has no get_summary_fields method")
             return {}
         return obj.get_summary_fields()
+
+    def to_representation(self, obj):
+        ret = super().to_representation(obj)
+
+        for key in obj.encrypted_fields:
+            if key in ret:
+                ret[key] = ENCRYPTED_STRING
+
+        return ret
+
+    def update(self, instance, validated_data):
+        # We don't want the $encrypted$ fields going back to the model
+        for key in self.Meta.model.encrypted_fields:
+            new_field = validated_data.get(key, None)
+            if new_field and new_field == ENCRYPTED_STRING:
+                validated_data.pop(key, None)
+
+        return super().update(instance, validated_data)
 
 
 class NamedCommonModelSerializer(CommonModelSerializer):
