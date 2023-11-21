4aded89507193f4437a1ceba77a99d4335c22e99
John Westcott IV <john.westcott.iv@redhat.com>
2023-11-21 15:42:06 -0500

    Fix stacks raised by get_authenticator_plugin

--- ansible_base/authentication/backend.py
+++ ansible_base/authentication/backend.py
@@ -18,10 +18,18 @@ class AnsibleBaseAuth(ModelBackend):
         for database_authenticator in Authenticator.objects.filter(enabled=True):
             # Either get the existing object out of the backends or get a new one for us
             if database_authenticator.id not in authentication_backends:
-                authentication_backends[database_authenticator.id] = get_authenticator_plugin(database_authenticator.type)
+                try:
+                    authentication_backends[database_authenticator.id] = get_authenticator_plugin(database_authenticator.type)
+                except ImportError:
+                    continue
             else:
                 if authentication_backends[database_authenticator.id].type != database_authenticator.type:
-                    authentication_backends[database_authenticator.id] = get_authenticator_plugin(database_authenticator.type)
+                    try:
+                        authentication_backends[database_authenticator.id] = get_authenticator_plugin(database_authenticator.type)
+                    except ImportError:
+                        # Since we failed to load this authenticator_plugin lets take it out of the cache
+                        del authentication_backends[database_authenticator.id]
+                        continue
             authenticator_object = authentication_backends[database_authenticator.id]
             authenticator_object.update_if_needed(database_authenticator)
             user = authenticator_object.authenticate(request, *args, **kwargs)
---
+++ ansible_base/tests/unit/serializers/test_authenticator.py
@@ -0,0 +1,19 @@
+import pytest
+
+from ansible_base.serializers import AuthenticatorSerializer
+
+
+@pytest.mark.django_db
+def test_removed_authenticator_plugin(ldap_authenticator):
+    serializer = AuthenticatorSerializer()
+    item = serializer.to_representation(ldap_authenticator)
+    assert 'error' not in item
+    assert 'configuration' in item
+    assert item['configuration'] != {}
+
+    # Change the type of the LDAP authenticator
+    ldap_authenticator.type = 'junk'
+    item = serializer.to_representation(ldap_authenticator)
+    assert 'error' in item
+    assert 'configuration' in item
+    assert item['configuration'] == {}
