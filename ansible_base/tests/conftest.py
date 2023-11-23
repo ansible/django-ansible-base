2a10bf4cfa50745733c006a268adab6f4c0ac109
Rick Elrod <rick@elrod.me>
2023-11-23 04:44:14 -0600

    Some coverage, pending a few questions
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- ansible_base/tests/conftest.py
+++ ansible_base/tests/conftest.py
@@ -1,6 +1,12 @@
+import datetime
 import uuid
+from collections import namedtuple
 
 import pytest
+from cryptography import x509
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.x509.oid import NameOID
 
 
 def copy_fixture(copies=1):
@@ -85,6 +91,74 @@ def shut_up_logging():
 
 
 @pytest.fixture
+def rsa_keypair_factory():
+    """
+    This fixture returns a factory function that creates a new RSA keypair every time it is called.
+
+    This is useful for tests that need to create multiple RSA keypairs. @copy_fixture isn't suitable
+    because it only ends up creating one keypair, so this avoids the issue.
+
+    A default instance of this fixture is available as rsa_keypair.
+    """
+
+    def rsa_keypair():
+        private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+        public_key = private_key.public_key()
+        private_key_bytes = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption(),
+        ).decode("utf-8")
+        public_key_bytes = public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo,
+        ).decode("utf-8")
+        RSAKeyPair = namedtuple("RSAKeyPair", ["private", "public"])
+        return RSAKeyPair(private=private_key_bytes, public=public_key_bytes)
+
+    return rsa_keypair
+
+
+@pytest.fixture
+def rsa_keypair(rsa_keypair_factory):
+    return rsa_keypair_factory()
+
+
+@copy_fixture(copies=3)
+@pytest.fixture
+def rsa_keypair_with_cert(rsa_keypair_factory):
+    rsa_keypair = rsa_keypair_factory()
+    private_key = serialization.load_pem_private_key(rsa_keypair.private.encode("utf-8"), password=None)
+    public_key = serialization.load_pem_public_key(rsa_keypair.public.encode("utf-8"))
+    subject = issuer = x509.Name(
+        [
+            x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
+            x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"California"),
+            x509.NameAttribute(NameOID.LOCALITY_NAME, u"San Francisco"),
+            x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"My Company"),
+            x509.NameAttribute(NameOID.COMMON_NAME, u"mycompany.com"),
+        ]
+    )
+    certificate = (
+        x509.CertificateBuilder()
+        .subject_name(subject)
+        .issuer_name(issuer)
+        .public_key(public_key)
+        .serial_number(x509.random_serial_number())
+        .not_valid_before(datetime.datetime.utcnow())
+        .not_valid_after(datetime.datetime.utcnow() + datetime.timedelta(days=365))
+        .add_extension(
+            x509.SubjectAlternativeName([x509.DNSName(u"mycompany.com")]),
+            critical=False,
+        )
+        .sign(private_key, hashes.SHA256())
+    )
+    certificate_bytes = certificate.public_bytes(serialization.Encoding.PEM).decode("utf-8")
+    RSAKeyPairWithCert = namedtuple("RSAKeyPairWithCert", ["private", "public", "certificate"])
+    return RSAKeyPairWithCert(private=rsa_keypair.private, public=rsa_keypair.public, certificate=certificate_bytes)
+
+
+@pytest.fixture
 def ldap_configuration():
     return {
         "SERVER_URI": ["ldap://ldap06.example.com:389"],
@@ -113,6 +187,48 @@ def ldap_authenticator(ldap_configuration):
         remove_users=True,
         type="ansible_base.authenticator_plugins.ldap",
         configuration=ldap_configuration,
+    )
+    yield authenticator
+    authenticator.delete()
+
+
+@pytest.fixture
+def saml_configuration(rsa_keypair_with_cert, rsa_keypair_with_cert_1):
+    return {
+        "SP_ENTITY_ID": "aap_gateway",
+        "SP_PUBLIC_CERT": rsa_keypair_with_cert.certificate,
+        "SP_PRIVATE_KEY": rsa_keypair_with_cert.private,
+        "ORG_INFO": {"en-US": {"url": "http://localhost", "name": "test app", "displayname": "Test App"}},
+        "TECHNICAL_CONTACT": {'givenName': "Technical Doe", 'emailAddress': "tdoe@example.com"},
+        "SUPPORT_CONTACT": {'givenName': "Support Doe", 'emailAddress': "sdoe@example.com"},
+        # "ENABLED_IDPS": ,
+        "SP_EXTRA": {"requestedAuthnContext": False},
+        "SECURITY_CONFIG": {},
+        "EXTRA_DATA": [],
+        "IDP_URL": "https://idp.example.com/idp/profile/SAML2/Redirect/SSO",
+        "IDP_X509_CERT": rsa_keypair_with_cert_1.certificate,
+        "IDP_ENTITY_ID": "https://idp.example.com/idp/shibboleth",
+        "IDP_GROUPS": "groups",
+        "IDP_ATTR_EMAIL": "email",
+        "IDP_ATTR_USERNAME": "username",
+        "IDP_ATTR_LAST_NAME": "last_name",
+        "IDP_ATTR_FIRST_NAME": "first_name",
+        "IDP_ATTR_USER_PERMANENT_ID": "user_permanent_id",
+    }
+
+
+@pytest.fixture
+def saml_authenticator(saml_configuration):
+    from ansible_base.models import Authenticator
+
+    authenticator = Authenticator.objects.create(
+        name="Test SAML Authenticator",
+        enabled=True,
+        create_objects=True,
+        users_unique=False,
+        remove_users=True,
+        type="ansible_base.authenticator_plugins.saml",
+        configuration=saml_configuration,
     )
     yield authenticator
     authenticator.delete()
