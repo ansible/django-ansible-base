b994906e33453433c947a0234f9926332ba05779
Doston Toirov <31990136+Dostonbek1@users.noreply.github.com>
2024-01-09 08:38:51 -0500

    [AAP-19305] fix 500 error on /authenticators/ DELETE endpoint (#60)
    * customize destroy mixin to return meaningful error messages

--- ansible_base/views/authenticator.py
+++ ansible_base/views/authenticator.py
@@ -1,8 +1,13 @@
-from rest_framework import permissions
+import logging
+
+from rest_framework import permissions, status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from ansible_base.models import Authenticator, AuthenticatorMap
+from ansible_base.models import Authenticator, AuthenticatorMap, AuthenticatorUser
 from ansible_base.serializers import AuthenticatorMapSerializer, AuthenticatorSerializer
+
+logger = logging.getLogger('ansible_base.views.authenticator')
 
 
 class AuthenticatorViewSet(ModelViewSet):
@@ -13,6 +18,18 @@ class AuthenticatorViewSet(ModelViewSet):
     queryset = Authenticator.objects.all()
     serializer_class = AuthenticatorSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        users_exist = AuthenticatorUser.objects.filter(provider_id=instance.slug).exists()
+        if users_exist:
+            logger.info("Found existing users from the authenticator")
+            return Response(
+                status=status.HTTP_409_CONFLICT, data={"details": "Authenticator cannot be deleted, as users from the authenticator exist in the system"}
+            )
+        else:
+            logger.info(f"Deleting authenticator with ID={instance.id}")
+            return super().destroy(request, *args, **kwargs)
 
 
 class AuthenticatorAuthenticatorMapViewSet(ModelViewSet):
