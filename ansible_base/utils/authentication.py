346a33a90302571ac3b5f941ec6df9079b972599
John Westcott IV <john.westcott.iv@redhat.com>
2024-01-09 13:38:07 -0500

    Add is_external_account to authentication utils

--- ansible_base/utils/authentication.py
+++ ansible_base/utils/authentication.py
@@ -2,7 +2,7 @@ import logging
 
 from rest_framework.serializers import ValidationError
 
-from ansible_base.models import Authenticator
+from ansible_base.models import Authenticator, AuthenticatorUser
 from ansible_base.utils.settings import get_setting
 from ansible_base.utils.validation import validate_url
 
@@ -46,3 +46,21 @@ def generate_ui_auth_data():
         logger.exception('LOGIN_REDIRECT_OVERRIDE was set but was not a valid URL, ignoring')
 
     return response
+
+
+def is_external_account(user) -> bool:
+    # True if the user is associated with any external login source
+    # False if the user is associated only with the local
+    # None if there is no user
+
+    if not user:
+        return None
+
+    authenticator_users = AuthenticatorUser.objects.filter(user_id=user.id)
+    for auth_user in authenticator_users:
+        provider = auth_user.provider
+        if provider.type != 'ansible_base.authenticator_plugins.local':
+            return True
+
+    # This user was not associated with any providers that were not the local provider
+    return False
