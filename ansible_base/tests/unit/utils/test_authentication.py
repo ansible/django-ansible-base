7e2599c05d17841566c779dce2ed3545ac4b65e8
John Westcott IV <john.westcott.iv@redhat.com>
2024-01-10 16:19:51 -0500

    Modify LOGIN_REDIRECT_URL to use get_setting function

--- ansible_base/tests/unit/utils/test_authentication.py
+++ ansible_base/tests/unit/utils/test_authentication.py
@@ -30,28 +30,9 @@ def test_generate_ui_auth_data_valid_login_redirect():
     }
 
 
-redirect_url = 'example.com'
-
-
-def set_login_redirect():
-    return redirect_url
-
-
-@override_settings(LOGIN_REDIRECT_OVERRIDE='ansible_base.tests.unit.utils.test_authentication.set_login_redirect')
-@pytest.mark.django_db
-def test_generate_ui_auth_data_valid_login_redirect_function():
-    response = generate_ui_auth_data()
-    assert response == {
-        'login_redirect_override': redirect_url,
-        'passwords': [],
-        'show_login_form': False,
-        'ssos': [],
-    }
-
-
 @mock.patch("ansible_base.utils.authentication.logger")
 @override_settings(LOGIN_REDIRECT_OVERRIDE='nonsense')
 @pytest.mark.django_db
 def test_generate_ui_auth_data_invalid_login_redirect_function(logger):
     generate_ui_auth_data()
-    logger.exception.assert_called_with('LOGIN_REDIRECT_OVERRIDE was set but was not a valid URL and calling it as a function failed (see exception), ignoring')
+    logger.exception.assert_called_with('LOGIN_REDIRECT_OVERRIDE was set but was not a valid URL, ignoring')
