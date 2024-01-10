6f44e655cf7f4f6d5e1c3999d4cbf4a367e000e5
John Westcott IV <john.westcott.iv@redhat.com>
2024-01-10 16:19:51 -0500

    Modify LOGIN_REDIRECT_URL to use get_setting function

--- ansible_base/utils/authentication.py
+++ ansible_base/utils/authentication.py
@@ -1,10 +1,9 @@
-import importlib
 import logging
 
-from django.conf import settings
 from rest_framework.serializers import ValidationError
 
 from ansible_base.models import Authenticator
+from ansible_base.utils.settings import get_setting
 from ansible_base.utils.validation import validate_url
 
 logger = logging.getLogger('ansible_base.utils.authentication')
@@ -39,18 +38,11 @@ def generate_ui_auth_data():
         else:
             logger.error(f"Don't know how to handle authenticator of type {authenticator.type}")
 
-    if hasattr(settings, 'LOGIN_REDIRECT_OVERRIDE'):
-        try:
-            validate_url(url=settings.LOGIN_REDIRECT_OVERRIDE, allow_plain_hostname=True)
-            response['login_redirect_override'] = settings.LOGIN_REDIRECT_OVERRIDE
-        except ValidationError:
-            # Maybe we are pointing to function
-            try:
-                module_name, _, function_name = settings.LOGIN_REDIRECT_OVERRIDE.rpartition('.')
-                the_function = getattr(importlib.import_module(module_name), function_name)
-                redirect_url = the_function()
-                response['login_redirect_override'] = redirect_url
-            except Exception:
-                logger.exception('LOGIN_REDIRECT_OVERRIDE was set but was not a valid URL and calling it as a function failed (see exception), ignoring')
+    try:
+        login_redirect_override = get_setting('LOGIN_REDIRECT_OVERRIDE', None)
+        validate_url(url=login_redirect_override, allow_plain_hostname=True)
+        response['login_redirect_override'] = login_redirect_override
+    except ValidationError:
+        logger.exception('LOGIN_REDIRECT_OVERRIDE was set but was not a valid URL, ignoring')
 
     return response
