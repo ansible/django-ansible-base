ff5fbdeb8d61031328ee1b2bc04d9e37afbb4681
Alan Rominger <arominge@redhat.com>
2024-01-15 14:22:16 -0500

    Fix isort

--- test_app/admin.py
+++ test_app/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 from test_app.models import EncryptionModel, Organization, Team
 
-
 admin.site.register(EncryptionModel)
 admin.site.register(Organization)
 admin.site.register(Team)
