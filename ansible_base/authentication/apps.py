e97933c7caec85aa111e72017e4c313087917f2d
John Westcott IV <john.westcott.iv@redhat.com>
2024-01-17 14:40:58 -0500

    Give authentication app a verbose name for admin pages

--- ansible_base/authentication/apps.py
+++ ansible_base/authentication/apps.py
@@ -6,3 +6,4 @@ import ansible_base.lib.checks  # noqa: F401 - register checks
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ansible_base.authentication'
+    verbose_name = 'Pluggable Authentication'
