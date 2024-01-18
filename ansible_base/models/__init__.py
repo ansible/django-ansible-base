5027249d6768b4da3c65eb30e4776a40bc4ff7c0
Alan Rominger <arominge@redhat.com>
2024-01-18 10:25:07 -0500

    Move Team model to a new module

--- ansible_base/models/__init__.py
+++ ansible_base/models/__init__.py
@@ -1,6 +1,7 @@
 from .authenticator import Authenticator
 from .authenticator_map import AuthenticatorMap
-from .organization import AbstractOrganization, AbstractTeam
+from .organization import AbstractOrganization
+from .team import AbstractTeam
 from .social_auth import AuthenticatorUser
 
 __all__ = (
--- ansible_base/models/organization.py
+++ ansible_base/models/organization.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from .common import NamedCommonModel, UniqueNamedCommonModel
+from .common import UniqueNamedCommonModel
 
 
 class AbstractOrganization(UniqueNamedCommonModel):
@@ -22,32 +22,4 @@ class AbstractOrganization(UniqueNamedCommonModel):
         settings.AUTH_USER_MODEL,
         related_name="organizations",
         help_text=_("The list of users in this organization."),
-    )
-
-
-class AbstractTeam(NamedCommonModel):
-    """
-    An abstract base class for teams.
-    A team groups users that work on the same resources, so that permissions can be assigned efficiently.
-    """
-
-    class Meta:
-        abstract = True
-        unique_together = [('organization', 'name')]
-        ordering = ('organization__name', 'name')
-
-    description = models.TextField(
-        null=False,
-        blank=True,
-        default="",
-        help_text=_("The team description."),
-    )
-
-    organization = models.ForeignKey(
-        settings.ANSIBLE_BASE_ORGANIZATION_MODEL,
-        blank=False,
-        null=False,
-        on_delete=models.CASCADE,
-        related_name="teams",
-        help_text=_("The organization of this team."),
     )
