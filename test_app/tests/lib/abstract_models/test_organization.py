9c06c5fa15e0f518bfad0fc4a25778e42169e709
Rick Elrod <rick@elrod.me>
2024-01-31 12:17:24 +0100

    Drop users from abstract org (#113)
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- test_app/tests/lib/abstract_models/test_organization.py
+++ test_app/tests/lib/abstract_models/test_organization.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 import pytest
-from django.contrib.auth import get_user_model
 from django.db import IntegrityError
 
 from test_app.models import Organization, Team
@@ -24,15 +23,6 @@ def test_organization_model_unique():
     Organization.objects.create(name="acme", description="ACME Corp.")
     with pytest.raises(IntegrityError):
         Organization.objects.create(name="acme", description="Second ACME Corp.")
-
-
-@pytest.mark.django_db
-def test_organization_model_users():
-    user = get_user_model().objects.create(username="alice")
-    org = Organization.objects.create(name="acme")
-    org.users.add(user)
-
-    assert list(user.organizations.all()) == [org]
 
 
 @pytest.mark.django_db
