3cb9379a5896a09b3be3ecc879b478a5729c2bf4
John Westcott IV <john.westcott.iv@redhat.com>
2024-01-31 15:31:53 -0500

    Convert back to setuptools-scm

--- ansible_base/__init__.py
+++ ansible_base/__init__.py
@@ -1,15 +1,18 @@
 try:
-    from ._version import version
-except ImportError:
-    version = 'Unknown'
+    from ._version import __version__, __version_tuple__  # noqa: F401
+except Exception:
+    import datetime
+    import subprocess
+    from typing import Tuple, Union
 
-from typing import Tuple, Union
+    calver_now = datetime.datetime.now().strftime("%Y.%m.%d")
+    shaw = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
 
-VERSION_TUPLE = Tuple[Union[int, str], ...]
+    VERSION_TUPLE = Tuple[Union[int, str], ...]
 
-__version__: str
-__version_tuple__: VERSION_TUPLE
-version_tuple: VERSION_TUPLE
+    __version__: str
+    __version_tuple__: VERSION_TUPLE
+    version_tuple: VERSION_TUPLE
 
-__version__ = version
-__version_tuple__ = version_tuple = tuple(version.split('.'))
+    __version__ = f'{calver_now}-{shaw}'
+    __version_tuple__ = version_tuple = tuple(__version__.split('.'))
--- ansible_base/_version.py
+++
@@ -1,6 +0,0 @@
-import datetime
-import subprocess
-
-calver_now = datetime.datetime.now().strftime("%Y.%m.%d")
-shaw = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
-version = f'{calver_now}-{shaw}'
