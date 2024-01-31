501b190758b8850205b2e14e4adfb78261ee8e25
Alan Rominger <arominge@redhat.com>
2024-01-31 11:57:01 -0500

    Sync migrations with recent model changes

--- test_app/migrations/0001_initial.py
+++ test_app/migrations/0001_initial.py
@@ -57,7 +57,6 @@ class Migration(migrations.Migration):
                 ('description', models.TextField(blank=True, default='', help_text='The organization description.')),
                 ('created_by', models.ForeignKey(default=None, editable=False, help_text='The user who created this resource', null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='%(app_label)s_%(class)s_created+', to=settings.AUTH_USER_MODEL)),
                 ('modified_by', models.ForeignKey(default=None, editable=False, help_text='The user who last modified this resource', null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='%(app_label)s_%(class)s_modified+', to=settings.AUTH_USER_MODEL)),
-                ('users', models.ManyToManyField(blank=True, help_text='The list of users in this organization.', related_name='organizations', to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'abstract': False,
