9a766a4f9b3f5ae413fe487dedc1d414d01f1d70
James Tanner <tanner.jc@gmail.com>
2024-02-06 09:19:42 -0500

    Fix logger and add i8ln
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- ansible_base/authentication/authenticator_plugins/github.py
+++ ansible_base/authentication/authenticator_plugins/github.py
@@ -1,36 +1,36 @@
 import logging
 
+from django.utils.translation import gettext_lazy as _
 from social_core.backends.github import GithubOAuth2
 
 from ansible_base.authentication.authenticator_plugins.base import AbstractAuthenticatorPlugin, BaseAuthenticatorConfiguration
 from ansible_base.authentication.social_auth import SocialAuthMixin, SocialAuthValidateCallbackMixin
 from ansible_base.lib.serializers.fields import CharField, URLField
 
-logger = logging.getLogger('ansible_base.authentication.authenticator_plugins.local')
+logger = logging.getLogger('ansible_base.authentication.authenticator_plugins.github')
 
 
 class GithubConfiguration(BaseAuthenticatorConfiguration):
     documenation_url = "https://python-social-auth.readthedocs.io/en/latest/backends/github.html"
 
     CALLBACK_URL = URLField(
-        help_text=(
+        help_text=_(
             'Provide this URL as the callback URL for your application as part of your registration process. Refer to the documentation for more detail.'
         ),
-        default='https://localhost/api/gateway/social/complete/',
         allow_null=False,
-        ui_field_label=('Callback URL'),
+        ui_field_label=_('Callback URL'),
     )
 
     KEY = CharField(
-        help_text=('The OAuth2 key (Client ID) from your GitHub developer application.'),
+        help_text=_('The OAuth2 key (Client ID) from your GitHub developer application.'),
         allow_null=False,
-        ui_field_label=('GitHub OAuth2 Key'),
+        ui_field_label=_('GitHub OAuth2 Key'),
     )
 
     SECRET = CharField(
-        help_text=('The OAuth2 secret (Client Secret) from your GitHub developer application.'),
+        help_text=_('The OAuth2 secret (Client Secret) from your GitHub developer application.'),
         allow_null=False,
-        ui_field_label=('GitHub OAuth2 Secret'),
+        ui_field_label=_('GitHub OAuth2 Secret'),
     )
 
 
