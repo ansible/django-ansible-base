e66b6fd25559d7379d8f8cb796c049fbe32a114d
John Westcott IV <john.westcott.iv@redhat.com>
2024-02-08 14:31:02 -0500

    Updating per PR feedback

--- ansible_base/lib/utils/views.py
+++ ansible_base/lib/utils/views.py
@@ -3,9 +3,7 @@ import logging
 import time
 
 from django.conf import settings
-from django.db import connection
 from django.utils.translation import gettext_lazy as _
-from rest_framework.exceptions import AuthenticationFailed, ParseError, PermissionDenied, UnsupportedMediaType
 from rest_framework.views import APIView
 
 from ansible_base.lib.utils.settings import get_function_from_setting, get_setting
@@ -20,48 +18,26 @@ class AnsibleBaseView(APIView):
         normal Django request, store time the request started.
         """
         self.time_started = time.time()
-        if get_setting('SQL_DEBUG', False):
-            self.queries_before = len(connection.queries)
 
-        # If there are any custom headers in REMOTE_HOST_HEADERS, make sure
-        # they respect the allowed proxy list
-        proxy_ip_allowed_list = get_setting('PROXY_IP_ALLOWED_LIST', None)
-        if proxy_ip_allowed_list and all(
-            [
-                request.environ.get('REMOTE_ADDR') not in proxy_ip_allowed_list,
-                request.environ.get('REMOTE_HOST') not in proxy_ip_allowed_list,
-            ]
-        ):
-            for custom_header in get_setting('REMOTE_HOST_HEADERS', None):
-                if custom_header.startswith('HTTP_'):
-                    request.environ.pop(custom_header, None)
-
-        drf_request = super().initialize_request(request, *args, **kwargs)
-        request.drf_request = drf_request
-        try:
-            request.drf_request_user = getattr(drf_request, 'user', False)
-        except AuthenticationFailed:
-            request.drf_request_user = None
-        except (PermissionDenied, ParseError) as exc:
-            request.drf_request_user = None
-            self.__init_request_error__ = exc
-        except UnsupportedMediaType as exc:
-            exc.detail = _(
-                'You did not use correct Content-Type in your HTTP request. If you are using our REST API, the Content-Type must be application/json'
-            )
-            self.__init_request_error__ = exc
-        return drf_request
+        return super().initialize_request(request, *args, **kwargs)
 
     def finalize_response(self, request, response, *args, **kwargs):
         response = super().finalize_response(request, response, *args, **kwargs)
 
-        if request.user.is_authenticated:
+        if request.user and request.user.is_authenticated:
             version = 'Unknown'
+            setting = 'ANSIBLE_BASE_PRODUCT_VERSION_FUNCTION'
+            the_function = None
             try:
-                the_function = get_function_from_setting('ANSIBLE_BASE_PRODUCT_VERSION_FUNCTION')
-                version = the_function()
+                the_function = get_function_from_setting(setting)
             except Exception:
-                logger.exception(_('ANSIBLE_BASE_PRODUCT_VERSION_FUNCTION was set but calling it as a function failed (see exception).'))
+                logger.exception(_('Failed to load function from {setting} (see exception)'.format(setting=setting)))
+
+            if the_function:
+                try:
+                    version = the_function()
+                except Exception:
+                    logger.exception(_('{setting} was set but calling it as a function failed (see exception).'.format(setting=setting)))
 
             response['X-API-Product-Version'] = version
 
@@ -72,12 +48,6 @@ class AnsibleBaseView(APIView):
         if time_started:
             time_elapsed = time.time() - self.time_started
             response['X-API-Time'] = '%0.3fs' % time_elapsed
-
-        if get_setting('SQL_DEBUG', False):
-            queries_before = getattr(self, 'queries_before', 0)
-            q_times = [float(q['time']) for q in connection.queries[queries_before:]]
-            response['X-API-Query-Count'] = len(q_times)
-            response['X-API-Query-Time'] = '%0.3fs' % sum(q_times)
 
         if getattr(self, 'deprecated', False):
             response['Warning'] = _('This resource has been deprecated and will be removed in a future release.')
@@ -93,8 +63,11 @@ if parent_view:
     try:
         module_name, junk, class_name = parent_view.rpartition('.')
         logger.debug(f"Trying to import parent view {class_name} from package {module_name}")
-        module = importlib.import_module(module_name, package=class_name)
-        parent_view_class = getattr(parent_view_class, class_name)
+        if not module_name or not class_name:
+            logger.error("ANSIBLE_BASE_CUSTOM_VIEW_PARENT must be in the format package.subpackage.view, defaulting to AnsibleBaseView")
+        else:
+            module = importlib.import_module(module_name, package=class_name)
+            parent_view_class = getattr(module, class_name)
     except ModuleNotFoundError:
         logger.error(f"Failed to find parent view class {parent_view}, defaulting to AnsibleBaseView")
     except ImportError:
