43c94c9cf61d0a28c027b1615735cb50611560bc
Lila <djyasin.ly@gmail.com>
2024-02-09 14:51:51 -0500

    Refine tests

--- test_app/tests/authentication/authenticator_plugins/test_tacacs.py
+++ test_app/tests/authentication/authenticator_plugins/test_tacacs.py
@@ -1,30 +1,42 @@
-# import logging
-# from collections import OrderedDict
-# from unittest import mock
-# from unittest.mock import MagicMock
+from unittest import mock
 
-# import pytest
-# import tacacs_plus
-# from django.urls import reverse
-# from rest_framework.serializers import ValidationError
+from django.urls import reverse
 
-# from ansible_base.authentication.authenticator_plugins.tacacs import AuthenticatorPlugin, TacacsConfiguration
-# from ansible_base.authentication.session import SessionAuthentication
+from ansible_base.authentication.session import SessionAuthentication
 
-# authenticated_test_page = "authenticator-list"
+authenticated_test_page = "authenticator-list"
 
-# @mock.patch("rest_framework.views.APIView.authentication_classes", [SessionAuthentication])
-# @mock.patch("ansible_base.authentication.authenticator_plugins.tacacs.AuthenticatorPlugin.authenticate")
-# def test_tacacs_auth_successful(authenticate, unauthenticated_api_client, AuthenticatorPlugin, user):
-#     """
-#     Test that a successful TACACS authentication returns a 200 on the /me endpoint.
 
-#     Here we mock the TACACS authentication backend to return a user.
-#     """
-#     client = unauthenticated_api_client
-#     authenticate.return_value = user
-#     client.login()
+@mock.patch("rest_framework.views.APIView.authentication_classes", [SessionAuthentication])
+@mock.patch("ansible_base.authentication.authenticator_plugins.tacacs.AuthenticatorPlugin.authenticate")
+def test_tacacs_auth_successful(authenticate, unauthenticated_api_client, tacacs_authenticator, user):
+    """
+    Test that a successful TACACS authentication returns a 200 on the /me endpoint.
 
-#     url = reverse(authenticated_test_page)
-#     response = client.get(url)
-#     assert response.status_code == 200
+    Here we mock the TACACS authentication backend to return a user.
+    """
+    client = unauthenticated_api_client
+    authenticate.return_value = user
+    did_login = client.login()
+    assert did_login, "Failed to login"
+
+    url = reverse(authenticated_test_page)
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@mock.patch("rest_framework.views.APIView.authentication_classes", [SessionAuthentication])
+@mock.patch("ansible_base.authentication.authenticator_plugins.tacacs.AuthenticatorPlugin.authenticate")
+def test_tacacs_auth_failure(authenticate, unauthenticated_api_client, tacacs_authenticator):
+    """
+    Test that a failed tacacs authentication returns a 401 on the /me endpoint.
+
+    Here we mock the tacacs authentication backend to return None.
+    """
+    client = unauthenticated_api_client
+    authenticate.return_value = None
+    client.login()
+
+    url = reverse(authenticated_test_page)
+    response = client.get(url)
+    assert response.status_code == 401
