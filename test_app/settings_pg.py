ec85d47df265d43b39a5826ec4a71c59b0a747ea
Alan Rominger <arominge@redhat.com>
2024-02-23 15:00:21 -0500

    try same name database

--- test_app/settings_pg.py
+++ test_app/settings_pg.py
@@ -9,7 +9,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "HOST": os.getenv("DB_HOST", "127.0.0.1"),
         "PORT": os.getenv("DB_PORT", 5432),
-        "USER": os.getenv("DB_USER", "ansibull"),
+        "USER": os.getenv("DB_USER", "dab"),
         "PASSWORD": os.getenv("DB_PASSWORD", "dabing"),
         "NAME": os.getenv("DB_NAME", "dab"),
     }
