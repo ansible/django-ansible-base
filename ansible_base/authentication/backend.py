da681a44d9c6c7353999e4cfd93443ec4a3b55a4
David Newswanger <dnewswan@redhat.com>
2024-02-28 14:08:49 -0600

    fix tests

--- ansible_base/authentication/backend.py
+++ ansible_base/authentication/backend.py
@@ -1,5 +1,6 @@
 import logging
 from collections import OrderedDict
+from functools import lru_cache
 
 from django.contrib.auth.backends import ModelBackend
 
@@ -8,32 +9,28 @@ from ansible_base.authentication.models import Authenticator
 
 logger = logging.getLogger('ansible_base.authentication.backend')
 
-authentication_backends = OrderedDict()
-last_authenticator_update = None
 
-
-def update_authenticator_cache():
-    global last_authenticator_update
-    global authentication_backends
-    last_modified = Authenticator.objects.values("modified_on").order_by("-modified_on").first()["modified_on"]
-
-    if last_modified == last_authenticator_update:
-        return
-
-    last_authenticator_update = last_modified
+@lru_cache(maxsize=1)
+def get_authentication_backends(last_updated):
+    # last_updated is primarily here as a cache busting mechanism
     authentication_backends = OrderedDict()
 
     for database_authenticator in Authenticator.objects.filter(enabled=True):
-        authentication_backends[database_authenticator.id] = get_authenticator_plugin(database_authenticator.type)
+        try:
+            authentication_backends[database_authenticator.id] = get_authenticator_plugin(database_authenticator.type)
+        except ImportError:
+            continue
         authenticator_object = authentication_backends[database_authenticator.id]
         authenticator_object.update_if_needed(database_authenticator)
+    return authentication_backends
 
 
 class AnsibleBaseAuth(ModelBackend):
     def authenticate(self, request, *args, **kwargs):
         logger.debug("Starting AnsibleBaseAuth authentication")
-        update_authenticator_cache()
-        for k, authenticator_object in authentication_backends.items():
+        last_modified = Authenticator.objects.values("modified_on").order_by("-modified_on").first()["modified_on"]
+
+        for k, authenticator_object in get_authentication_backends(last_modified).items():
             user = authenticator_object.authenticate(request, *args, **kwargs)
             if user:
                 # The local authenticator handles this but we want to check this for other authentication types
