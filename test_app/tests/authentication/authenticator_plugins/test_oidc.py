02f2e383ef689998a660a59d5c731ce316158eed
James Tanner <tanner.jc@gmail.com>
2024-02-28 16:05:45 -0500

    Add range of tests for oidc endpoint url validation.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- test_app/tests/authentication/authenticator_plugins/test_oidc.py
+++ test_app/tests/authentication/authenticator_plugins/test_oidc.py
@@ -1,5 +1,6 @@
 from unittest import mock
 
+import pytest
 from django.urls import reverse
 
 from ansible_base.authentication.session import SessionAuthentication
@@ -36,3 +37,61 @@ def test_oidc_auth_failed(authenticate, unauthenticated_api_client, oidc_authent
     url = reverse(authenticated_test_page)
     response = client.get(url)
     assert response.status_code == 401
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "endpoint_url, expected_status_code, expected_error",
+    [
+        (None, 400, {'OIDC_ENDPOINT': ['This field may not be null.']}),
+        ('', 400, {'OIDC_ENDPOINT': ['This field may not be blank.']}),
+        ('foobar', 400, {'OIDC_ENDPOINT': ['Enter a valid URL.']}),
+        ('123456', 400, {'OIDC_ENDPOINT': ['Enter a valid URL.']}),
+        ('/////', 400, {'OIDC_ENDPOINT': ['Enter a valid URL.']}),
+        ('...', 400, {'OIDC_ENDPOINT': ['Enter a valid URL.']}),
+        ('192.168.1.1', 400, {'OIDC_ENDPOINT': ['Enter a valid URL.']}),
+        ('0.0.0.0', 400, {'OIDC_ENDPOINT': ['Enter a valid URL.']}),
+        ('httpXX://foobar', 400, {'OIDC_ENDPOINT': ['Enter a valid URL.']}),
+        # ('http://foobar::not::ip::v6', 400, {'OIDC_ENDPOINT': ['Enter a valid URL.']}),
+        # ('http://foobar:ABDC', 400, {'OIDC_ENDPOINT': ['Enter a valid URL.']}),
+        ('http://foobar', 201, {}),
+        ('http://foobar:80', 201, {}),
+        ('https://foobar', 201, {}),
+        ('https://foobar:443', 201, {}),
+        # ('http://[::1]', 201, {}),
+        # ('http://[::1]:80', 201, {}),
+        # ('http://[::192.9.5.5]/', 201, {}),
+        # ('http://[::FFFF:129.144.52.38]:80', 201, {}),
+    ],
+)
+def test_oidc_endpoint_url_validation(
+    admin_api_client,
+    endpoint_url,
+    expected_status_code,
+    expected_error,
+):
+
+    config = {
+        "OIDC_ENDPOINT": endpoint_url,
+        "VERIFY_SSL": True,
+        "KEY": "12345",
+        "SECRET": "abcdefg12345",
+    }
+
+    data = {
+        "name": "OIDC TEST",
+        "enabled": True,
+        "create_objects": True,
+        "users_unique": False,
+        "remove_users": True,
+        "configuration": config,
+        "type": "ansible_base.authentication.authenticator_plugins.oidc",
+    }
+
+    url = reverse("authenticator-list")
+    response = admin_api_client.post(url, data=data, format="json")
+    assert response.status_code == expected_status_code
+    if expected_error:
+        assert response.json() == expected_error
+    else:
+        assert response.json()['configuration']['OIDC_ENDPOINT'] == endpoint_url
