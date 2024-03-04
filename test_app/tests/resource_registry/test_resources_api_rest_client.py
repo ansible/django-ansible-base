0dc6b80381b4b760134e2724eede56d631bb93cd
David Newswanger <dnewswan@redhat.com>
2024-03-04 15:09:48 -0600

    fix the tests

--- test_app/tests/resource_registry/test_resources_api_rest_client.py
+++ test_app/tests/resource_registry/test_resources_api_rest_client.py
@@ -1,16 +1,53 @@
 import uuid
 
 import pytest
+from crum import impersonate
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
 from ansible_base.resource_registry.models import Resource, service_id
 from ansible_base.resource_registry.rest_client import ResourceAPIClient
 
 
 @pytest.fixture
-def resource_client(admin_user, live_server, local_authenticator) -> ResourceAPIClient:
+def system_user():
+    """
+    Generate the system user so that CommonModel.save() doesn't cry.
+    """
+    UserModel = get_user_model()
+    if UserModel.objects.filter(username=settings.SYSTEM_USERNAME).exists():
+        yield UserModel.objects.get(username=settings.SYSTEM_USERNAME)
+    else:
+        with impersonate(None):
+            user = UserModel(username=settings.SYSTEM_USERNAME)
+            user.save(non_existent_user_fatal=False)
+            yield user
+
+
+@pytest.fixture
+def resource_client(system_user, admin_user, live_server, local_authenticator, transactional_db) -> ResourceAPIClient:
+    """
+    Okay, there's a lot going on with this fixture, so let me explain.
+
+    The `live_server` fixture is a weird one. The tests run in a separate thread from the
+    django instance[1], so migration data doesn't exist. Because of that, we don't get
+    the `_system user`, which comes from migrations[2], so that has to be created explicitly here
+    because without the _system user, the system throws "ValueError: Unable to save model
+    without user!" any time any model that inherits from CommonModel is saved. Theoretically
+    the `django_db_serialized_rollback` should solve this problem, but when I added it
+    everything completely breaks.
+
+    The `transactional_db`[1] fixture is used to rollback tests when the django server is
+    running in a different thread. It's slower than the `db`, but using the `db` fixture
+    causes weird failures in other tests.
+
+    [1] https://pytest-django.readthedocs.io/en/latest/helpers.html#live-server
+    [2] test_app/migrations/0003_create_system_user.py
+    """
     return ResourceAPIClient(live_server.url, "/api/v1/service-index/", {"auth": ("admin", "password")})
 
 
+@pytest.mark.django_db
 def test_service_metadata(resource_client):
     """Test that the resource list is working."""
     resp = resource_client.get_service_metadata()
@@ -19,6 +56,7 @@ def test_service_metadata(resource_client):
     assert resp.json()["service_id"] == str(service_id())
 
 
+@pytest.mark.django_db
 def test_create_resource(resource_client):
     data = resource_client.ResourceRequestBody(resource_type="shared.user", resource_data={"username": "mr_dab"})
     resp = resource_client.create_resource(data)
@@ -40,6 +78,7 @@ def test_create_resource(resource_client):
     assert resp.json()["service_id"] == new_service_id
 
 
+@pytest.mark.django_db
 def test_get_resource(resource_client, organization):
     ansible_id = str(Resource.get_resource_for_object(organization).ansible_id)
     resp = resource_client.get_resource(ansible_id)
@@ -48,6 +87,7 @@ def test_get_resource(resource_client, organization):
     assert resp.json()["name"] == organization.name
 
 
+@pytest.mark.django_db
 def test_update_resource(resource_client, organization):
     ansible_id = str(Resource.get_resource_for_object(organization).ansible_id)
     data = resource_client.ResourceRequestBody(resource_data={"name": "my_new_org"})
@@ -74,6 +114,7 @@ def test_update_resource(resource_client, organization):
     assert resp.json()["service_id"] == new_service_id
 
 
+@pytest.mark.django_db
 def test_delete_resource(resource_client, organization):
     ansible_id = str(Resource.get_resource_for_object(organization).ansible_id)
     resp = resource_client.delete_resource(ansible_id)
@@ -84,6 +125,7 @@ def test_delete_resource(resource_client, organization):
     assert resp.status_code == 404
 
 
+@pytest.mark.django_db
 def test_list_resources(resource_client, organization):
     ansible_id = str(Resource.get_resource_for_object(organization).ansible_id)
     resp = resource_client.list_resources()
@@ -96,6 +138,7 @@ def test_list_resources(resource_client, organization):
     assert resp.json()["results"][0]["ansible_id"] == ansible_id
 
 
+@pytest.mark.django_db
 def test_get_resource_type(resource_client):
     resp = resource_client.get_resource_type("shared.organization")
 
@@ -103,6 +146,7 @@ def test_get_resource_type(resource_client):
     assert resp.json()["name"] == "shared.organization"
 
 
+@pytest.mark.django_db
 def test_list_resource_types(resource_client):
     resp = resource_client.list_resource_types()
     assert resp.status_code == 200
