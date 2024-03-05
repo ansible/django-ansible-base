14c97031f1c4df2376300fd7f98fb92dcaccf060
Alan Rominger <arominge@redhat.com>
2024-03-05 11:26:34 -0500

    Add app links to root API view (#197)

--- test_app/views.py
+++ test_app/views.py
@@ -50,10 +50,12 @@ class RelatedFieldsTestModelViewSet(TestAppViewSet):
 # create api root view from the router
 @api_view(['GET'])
 def api_root(request, format=None):
+    from ansible_base.authentication.urls import router as auth_router
+    from ansible_base.resource_registry.urls import service_router
     from test_app.router import router
 
     list_endpoints = {}
-    for url in router.urls:
+    for url in router.urls + auth_router.urls + service_router.urls:
         # only want "root" list views, for example:
         # want '^users/$' [name='user-list']
         # do not want '^users/(?P<pk>[^/.]+)/organizations/$' [name='user-organizations-list'],
