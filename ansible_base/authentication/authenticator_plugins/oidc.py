c1f2f388e73e88ad06165531ca05b0f55d0ef65f
James Tanner <tanner.jc@gmail.com>
2024-03-05 09:29:15 -0500

    Fill in help text.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- ansible_base/authentication/authenticator_plugins/oidc.py
+++ ansible_base/authentication/authenticator_plugins/oidc.py
@@ -14,6 +14,10 @@ logger = logging.getLogger('ansible_base.authentication.authenticator_plugins.oi
 class OpenIdConnectConfiguration(BaseAuthenticatorConfiguration):
     documentation_url = "https://python-social-auth.readthedocs.io/en/latest/backends/oidc.html"
 
+    #################################
+    # Minimal params
+    #################################
+
     OIDC_ENDPOINT = URLField(
         help_text=_("The URL for your OIDC provider including the path up to /.well-known/openid-configuration"),
         allow_null=False,
@@ -40,50 +44,13 @@ class OpenIdConnectConfiguration(BaseAuthenticatorConfiguration):
     )
 
     #################################
-    # MORE
+    # Additional params
     #################################
 
     PUBLIC_KEY = CharField(
         help_text=_("The public key from your IDP. Only necessary if using keycloak for OIDC."),
         allow_null=True,
         ui_field_label=_('OIDC Public Key'),
-    )
-
-    ID_TOKEN_MAX_AGE = IntegerField(
-        help_text=_(""),
-        default=600,
-        allow_null=True,
-        ui_field_label=_('OIDC token max age'),
-    )
-
-    REDIRECT_STATE = BooleanField(help_text=_(""), default=False, allow_null=True, ui_field_label=_("redirect state"))
-
-    ACCESS_TOKEN_METHOD = CharField(
-        help_text=_(""),
-        default="POST",
-        allow_null=True,
-        ui_field_label=_(""),
-    )
-
-    REVOKE_TOKEN_METHOD = CharField(
-        help_text=_(""),
-        default="GET",
-        allow_null=True,
-        ui_field_label=_(""),
-    )
-
-    ID_KEY = CharField(
-        help_text=_(""),
-        default="sub",
-        allow_null=True,
-        ui_field_label=_(""),
-    )
-
-    USERNAME_KEY = CharField(
-        help_text=_(""),
-        default="preferred_username",
-        allow_null=True,
-        ui_field_label=_(""),
     )
 
     ALGORITHM = CharField(
@@ -93,53 +60,95 @@ class OpenIdConnectConfiguration(BaseAuthenticatorConfiguration):
         ui_field_label=_('OIDC JWT Algorithm'),
     )
 
+    ID_TOKEN_MAX_AGE = IntegerField(
+        help_text=_("The maximum allowed age (in seconds) of the ID token. Tokens older than this will be rejected."),
+        default=600,
+        allow_null=True,
+        ui_field_label=_('OIDC Token Max Age'),
+    )
+
+    REDIRECT_STATE = BooleanField(
+        help_text=_("Enable or disable state parameter in the redirect URI. Recommended to be True for preventing CSRF attacks."),
+        default=False,
+        allow_null=True,
+        ui_field_label=_("Redirect State"),
+    )
+
+    ACCESS_TOKEN_METHOD = CharField(
+        help_text=_("The HTTP method to be used when requesting an access token. Typically 'POST' or 'GET'."),
+        default="POST",
+        allow_null=True,
+        ui_field_label=_("Access Token Method"),
+    )
+
+    REVOKE_TOKEN_METHOD = CharField(
+        help_text=_("The HTTP method to be used when revoking an access token. Typically 'POST' or 'GET'."),
+        default="GET",
+        allow_null=True,
+        ui_field_label=_("Revoke Token Method"),
+    )
+
+    ID_KEY = CharField(
+        help_text=_("The JSON key used to extract the user's ID from the ID token."),
+        default="sub",
+        allow_null=True,
+        ui_field_label=_("ID Key"),
+    )
+
+    USERNAME_KEY = CharField(
+        help_text=_("The JSON key used to extract the user's username from the ID token or userinfo endpoint."),
+        default="preferred_username",
+        allow_null=True,
+        ui_field_label=_("Username Key"),
+    )
+
     ID_TOKEN_ISSUER = CharField(
-        help_text=_(""),
+        help_text=_("Expected issuer ('iss') of the ID token. If set, it will be used to validate the issuer of the ID token."),
         default="",
         allow_null=True,
-        ui_field_label=_(""),
+        ui_field_label=_("ID Token Issuer"),
     )
 
     ACCESS_TOKEN_URL = URLField(
-        help_text=_(""),
+        help_text=_("The URL to obtain an access token from the OIDC provider."),
         default="",
         allow_null=True,
-        ui_field_label=_(""),
+        ui_field_label=_("Access Token URL"),
     )
 
     AUTHORIZATION_URL = URLField(
-        help_text=_(""),
+        help_text=_("The URL to redirect the user for OIDC provider authorization."),
         default="",
         allow_null=True,
-        ui_field_label=_(""),
+        ui_field_label=_("Authorization URL"),
     )
 
     REVOKE_TOKEN_URL = URLField(
-        help_text=_(""),
+        help_text=_("The URL to revoke tokens. Used in the token revocation flow."),
         default="",
         allow_null=True,
-        ui_field_label=_(""),
+        ui_field_label=_("Revoke Token URL"),
     )
 
     USERINFO_URL = URLField(
-        help_text=_(""),
+        help_text=_("The URL to retrieve user information from the OIDC provider."),
         default="",
         allow_null=True,
-        ui_field_label=_(""),
+        ui_field_label=_("Userinfo URL"),
     )
 
     JWKS_URI = URLField(
-        help_text=_(""),
+        help_text=_("The URL to retrieve the provider's public keys for verifying JWT signatures."),
         default="",
         allow_null=True,
-        ui_field_label=_(""),
+        ui_field_label=_("JWKS URI"),
     )
 
     TOKEN_ENDPOINT_AUTH_METHOD = CharField(
-        help_text=_(""),
+        help_text=_("The authentication method to use at the token endpoint. Common values are 'client_secret_post', 'client_secret_basic'."),
         default="",
         allow_null=True,
-        ui_field_label=_(""),
+        ui_field_label=_("Token Endpoint Auth Method"),
     )
 
 
