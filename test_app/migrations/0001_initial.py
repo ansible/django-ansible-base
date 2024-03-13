45b339098b877776dbd4f8a1081bcaab969da590
John Westcott IV <john.westcott.iv@redhat.com>
2024-03-13 14:06:35 -0400

    Squash test_app migrations

--- test_app/migrations/0001_initial.py
+++ test_app/migrations/0001_initial.py
@@ -8,6 +8,23 @@ import django.db.models.deletion
 import django.utils.timezone
 
 
+def create_system_user(apps, schema_editor):
+    """
+    Create the system user using the username in settings.
+
+    The user is self-referential in its created_by and modified_by fields.
+    It is inactive, only used for attributing internal changes to the system.
+    """
+    User = apps.get_model(settings.AUTH_USER_MODEL)
+
+    system_username = settings.SYSTEM_USERNAME
+    if not User.objects.filter(username=system_username).exists():
+        system_user = User.objects.create(username=system_username, is_active=False)
+        system_user.created_by = system_user
+        system_user.modified_by = system_user
+        system_user.save()
+
+
 class Migration(migrations.Migration):
 
     initial = True
@@ -31,8 +48,8 @@ class Migration(migrations.Migration):
                 ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
                 ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
-                ('created_on', models.DateTimeField(default=None, editable=False, help_text='The date/time this resource was created')),
-                ('modified_on', models.DateTimeField(default=None, editable=False, help_text='The date/time this resource was created')),
+                ('created', models.DateTimeField(auto_now_add=True, help_text='The date/time this resource was created')),
+                ('modified', models.DateTimeField(auto_now=True, help_text='The date/time this resource was created')),
                 ('created_by', models.ForeignKey(default=None, editable=False, help_text='The user who created this resource', null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='%(app_label)s_%(class)s_created+', to=settings.AUTH_USER_MODEL)),
                 ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
                 ('modified_by', models.ForeignKey(default=None, editable=False, help_text='The user who last modified this resource', null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='%(app_label)s_%(class)s_modified+', to=settings.AUTH_USER_MODEL)),
@@ -51,8 +68,8 @@ class Migration(migrations.Migration):
             name='Organization',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_on', models.DateTimeField(default=None, editable=False, help_text='The date/time this resource was created')),
-                ('modified_on', models.DateTimeField(default=None, editable=False, help_text='The date/time this resource was created')),
+                ('created', models.DateTimeField(auto_now_add=True, help_text='The date/time this resource was created')),
+                ('modified', models.DateTimeField(auto_now=True, help_text='The date/time this resource was created')),
                 ('name', models.CharField(help_text='The name of this resource', max_length=512, unique=True)),
                 ('description', models.TextField(blank=True, default='', help_text='The organization description.')),
                 ('created_by', models.ForeignKey(default=None, editable=False, help_text='The user who created this resource', null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='%(app_label)s_%(class)s_created+', to=settings.AUTH_USER_MODEL)),
@@ -66,8 +83,8 @@ class Migration(migrations.Migration):
             name='Team',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_on', models.DateTimeField(default=None, editable=False, help_text='The date/time this resource was created')),
-                ('modified_on', models.DateTimeField(default=None, editable=False, help_text='The date/time this resource was created')),
+                ('created', models.DateTimeField(auto_now_add=True, help_text='The date/time this resource was created')),
+                ('modified', models.DateTimeField(auto_now=True, help_text='The date/time this resource was created')),
                 ('name', models.CharField(help_text='The name of this resource', max_length=512)),
                 ('description', models.TextField(blank=True, default='', help_text='The team description.')),
                 ('created_by', models.ForeignKey(default=None, editable=False, help_text='The user who created this resource', null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='%(app_label)s_%(class)s_created+', to=settings.AUTH_USER_MODEL)),
@@ -84,8 +101,8 @@ class Migration(migrations.Migration):
             name='RelatedFieldsTestModel',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_on', models.DateTimeField(default=None, editable=False, help_text='The date/time this resource was created')),
-                ('modified_on', models.DateTimeField(default=None, editable=False, help_text='The date/time this resource was created')),
+                ('created', models.DateTimeField(auto_now_add=True, help_text='The date/time this resource was created')),
+                ('modified', models.DateTimeField(auto_now=True, help_text='The date/time this resource was created')),
                 ('created_by', models.ForeignKey(default=None, editable=False, help_text='The user who created this resource', null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='%(app_label)s_%(class)s_created+', to=settings.AUTH_USER_MODEL)),
                 ('modified_by', models.ForeignKey(default=None, editable=False, help_text='The user who last modified this resource', null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='%(app_label)s_%(class)s_modified+', to=settings.AUTH_USER_MODEL)),
                 ('more_teams', models.ManyToManyField(related_name='related_fields_test_model_more_teams', to='test_app.team')),
@@ -100,8 +117,8 @@ class Migration(migrations.Migration):
             name='EncryptionModel',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_on', models.DateTimeField(default=None, editable=False, help_text='The date/time this resource was created')),
-                ('modified_on', models.DateTimeField(default=None, editable=False, help_text='The date/time this resource was created')),
+                ('created', models.DateTimeField(auto_now_add=True, help_text='The date/time this resource was created')),
+                ('modified', models.DateTimeField(auto_now=True, help_text='The date/time this resource was created')),
                 ('name', models.CharField(help_text='The name of this resource', max_length=512)),
                 ('testing1', models.CharField(default='a', max_length=400, null=True)),
                 ('testing2', models.CharField(default='b', max_length=400, null=True)),
@@ -114,4 +131,5 @@ class Migration(migrations.Migration):
             name='encryptioner',
             field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='test_app.encryptionmodel'),
         ),
+        migrations.RunPython(create_system_user),
     ]
