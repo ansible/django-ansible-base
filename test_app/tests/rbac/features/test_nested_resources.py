4af9f8b72d47a396dc294697e8e5845f29743e82
Alan Rominger <arominge@redhat.com>
2024-03-27 14:24:17 -0400

    Fix missing signals for multiple layers of parents

--- test_app/tests/rbac/features/test_nested_resources.py
+++ test_app/tests/rbac/features/test_nested_resources.py
@@ -15,6 +15,15 @@ def collection(namespace):
     return CollectionImport.objects.create(name='bar', namespace=namespace)
 
 
+@pytest.fixture
+def org_collection_rd():
+    return RoleDefinition.objects.create_from_permissions(
+        permissions=['change_collectionimport', 'delete_collectionimport', 'add_collectionimport', 'view_collectionimport', 'view_namespace'],
+        name='collection-manager',
+        content_type=permission_registry.content_type_model.objects.get_for_model(Organization),
+    )
+
+
 def test_parent_resource_utils():
     assert permission_registry.get_parent_fd_name(CollectionImport) == 'namespace'
     assert permission_registry.get_parent_fd_name(Namespace) == 'organization'
@@ -27,19 +36,49 @@ def test_parent_resource_utils():
 
 
 @pytest.mark.django_db
-def test_grandparent_assignment(rando, organization, namespace, collection):
-    rd, _ = RoleDefinition.objects.get_or_create(
-        permissions=['change_collectionimport', 'delete_collectionimport', 'add_collectionimport', 'view_collectionimport', 'view_namespace'],
-        name='collection-manager',
-        content_type=permission_registry.content_type_model.objects.get_for_model(organization),
-    )
-    rd.give_permission(rando, organization)
+def test_grandparent_assignment(rando, organization, namespace, collection, org_collection_rd):
+    org_collection_rd.give_permission(rando, organization)
     assert rando.has_obj_perm(collection, 'change_collectionimport')
 
     assert rando.has_obj_perm(namespace, 'add_collectionimport')
     assert rando.has_obj_perm(organization, 'add_collectionimport')
 
     assert set(Organization.access_qs(rando, 'add_collectionimport')) == set([organization])
+
+
+@pytest.mark.django_db
+def test_create_grandchild_object(rando, organization, namespace, org_collection_rd):
+    org_collection_rd.give_permission(rando, organization)
+    collection = CollectionImport.objects.create(name='bar', namespace=namespace)
+    assert rando.has_obj_perm(collection, 'change_collectionimport')
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('parent', ['namespace', 'organization'])
+def test_move_grandchild_object(rando, org_collection_rd, parent):
+    orgs = [Organization.objects.create(name=f'foo-{i}') for i in range(2)]
+    namespaces = [Namespace.objects.create(name=f'foo-{i}', organization=orgs[i]) for i in range(2)]
+    collections = [CollectionImport.objects.create(name=f'bar-{i}', namespace=namespaces[i]) for i in range(2)]
+
+    org_collection_rd.give_permission(rando, orgs[0])
+    assert rando.has_obj_perm(collections[0], 'change_collectionimport')
+    assert not rando.has_obj_perm(collections[1], 'change_collectionimport')
+
+    if parent == 'namespace':
+        collections[0].namespace = namespaces[1]
+        collections[0].save()
+        collections[1].namespace = namespaces[0]
+        collections[1].save()
+    else:
+        namespaces[0].organization = orgs[1]
+        namespaces[0].save()
+        namespaces[1].organization = orgs[0]
+        namespaces[1].save()
+
+    # In both cases, the two collections have switched organizations
+    # and the rando user has permission to the first org, which now applies to the 2nd collection
+    assert not rando.has_obj_perm(collections[0], 'change_collectionimport')
+    assert rando.has_obj_perm(collections[1], 'change_collectionimport')
 
 
 @pytest.mark.django_db
