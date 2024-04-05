e1baa92a103c9002189f9b49a2f2107405d296c6
James Tanner <tanner.jc@gmail.com>
2024-04-05 08:47:24 -0400

    Re-organize.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- test_app/scripts/container_startup.sh
+++ test_app/scripts/container_startup.sh
@@ -10,9 +10,7 @@ echo "settings.DATABASE ..."
 $PYTHON manage.py shell -c 'from django.conf import settings; print(settings.DATABASES)'
 
 $PYTHON manage.py migrate
-set +e
-DJANGO_SUPERUSER_PASSWORD=password DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_EMAIL=admin@stuff.invalid $PYTHON manage.py createsuperuser --noinput
-set -e
+DJANGO_SUPERUSER_PASSWORD=password DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_EMAIL=admin@stuff.invalid $PYTHON manage.py createsuperuser --noinput || true
 $PYTHON manage.py authenticators --initialize
 $PYTHON manage.py create_demo_data
 $PYTHON manage.py runserver 0.0.0.0:8000
