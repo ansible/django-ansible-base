d108b0ed0c94182ba35c190cf4c2ca29a2aa0275
Rick Elrod <rick@elrod.me>
2024-04-08 08:22:39 -0500

    [dynamic_urls] Avoid masking import errors (#278)
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- ansible_base/lib/dynamic_config/dynamic_urls.py
+++ ansible_base/lib/dynamic_config/dynamic_urls.py
@@ -1,3 +1,4 @@
+import importlib.util
 import logging
 
 from django.conf import settings
@@ -11,10 +12,10 @@ for url_type in url_types:
 
 for app in getattr(settings, 'INSTALLED_APPS', []):
     if app.startswith('ansible_base.'):
-        try:
-            url_module = __import__(f'{app}.urls', fromlist=url_types)
-            logger.debug(f'Including URLS from {app}.urls')
-            for url_type in ['api_version_urls', 'root_urls', 'api_urls']:
-                globals()[url_type].extend(getattr(url_module, url_type, []))
-        except ImportError:
+        if not importlib.util.find_spec(f'{app}.urls'):
             logger.debug(f'Module {app} does not specify urls.py')
+            continue
+        url_module = __import__(f'{app}.urls', fromlist=url_types)
+        logger.debug(f'Including URLS from {app}.urls')
+        for url_type in ['api_version_urls', 'root_urls', 'api_urls']:
+            globals()[url_type].extend(getattr(url_module, url_type, []))
--- ansible_base/rest_pagination/urls.py
+++
@@ -1,7 +0,0 @@
-from ansible_base.rest_pagination.apps import RestPaginationConfig
-
-app_name = RestPaginationConfig.label
-
-api_version_urls = []
-root_urls = []
-api_urls = []
