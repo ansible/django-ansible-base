96e7caf0ba88be06db0cd2c8afdb260b155166dd
Alan Rominger <arominge@redhat.com>
2024-04-08 15:37:30 -0400

    Fix failing AWX checks due to schema issues

--- ansible_base/resource_registry/views.py
+++ ansible_base/resource_registry/views.py
@@ -143,8 +143,6 @@ class ResourceTypeViewSet(
 class ServiceMetadataView(
     AnsibleBaseDjangoAppApiView,
 ):
-    action = "service-metadata"
-
     permission_classes = [
         HasResourceRegistryPermissions,
     ]
@@ -173,8 +171,6 @@ class ValidateLocalUserView(AnsibleBaseDjangoAppApiView):
     """
     Validate a user's username and password.
     """
-
-    action = "validate-local-user"
     permission_classes = [
         HasResourceRegistryPermissions,
     ]
