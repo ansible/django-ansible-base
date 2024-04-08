28dd0d4beded72e54c70083df47be4558ea4a449
Alan Rominger <arominge@redhat.com>
2024-04-07 22:42:34 -0400

    Transaction-safe ObjectRole creation

---
+++ test_app/tests/rbac/models/test_object_role.py
@@ -0,0 +1,19 @@
+from unittest import mock
+
+import pytest
+
+from ansible_base.rbac import permission_registry
+from ansible_base.rbac.models import ObjectRole
+from test_app.models import User
+
+
+@pytest.mark.django_db
+def test_existing_object_role_race(inv_rd, inventory):
+    user1 = User.objects.create(username='user1')
+    inv_rd.give_permission(user1, inventory)
+
+    user2 = User.objects.create(username='user2')
+    with mock.patch('django.db.models.query.QuerySet.first', return_value=None):
+        assert ObjectRole.objects.filter(object_id=inventory.pk).first() is None  # sanity
+        inv_rd.give_permission(user2, inventory)
+    assert user2.has_obj_perm(inventory, 'change')
