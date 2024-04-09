28de08a7769216878e715cbfa0cabd21303d42ff
Alan Rominger <arominge@redhat.com>
2024-04-09 15:51:19 -0400

    Allow for customizing the automatic creator role

--- ansible_base/rbac/models.py
+++ ansible_base/rbac/models.py
@@ -70,7 +70,7 @@ class RoleDefinitionManager(models.Manager):
         has_permissions = set(RoleEvaluation.get_permissions(user, obj))
         has_permissions.update(user.singleton_permissions())
         if set(needed_perms) - set(has_permissions):
-            kwargs = {'permissions': needed_perms, 'name': f'{obj._meta.model_name}-creator-permission'}
+            kwargs = {'permissions': needed_perms, 'name': settings.ANSIBLE_BASE_ROLE_CREATOR_NAME.format(obj=obj, cls=type(obj))}
             defaults = {'content_type': ContentType.objects.get_for_model(obj)}
             try:
                 rd, _ = self.get_or_create(defaults=defaults, **kwargs)
