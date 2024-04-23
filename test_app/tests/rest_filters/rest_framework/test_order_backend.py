ebbb71778de5ff5b401774515bee8d2855a1c123
John Westcott IV <john.westcott.iv@redhat.com>
2024-04-23 11:10:39 -0400

    Make id the default order field

--- test_app/tests/rest_filters/rest_framework/test_order_backend.py
+++ test_app/tests/rest_filters/rest_framework/test_order_backend.py
@@ -3,9 +3,11 @@ from unittest.mock import MagicMock, Mock
 import pytest
 from django.core.exceptions import FieldError
 from rest_framework.exceptions import ParseError
+from rest_framework.viewsets import ModelViewSet
 
 from ansible_base.authentication.models import Authenticator
 from ansible_base.authentication.views import AuthenticatorViewSet
+from ansible_base.lib.utils.views.django_app_api import AnsibleBaseDjangoAppApiView
 from ansible_base.rest_filters.rest_framework.order_backend import OrderByBackend
 
 
@@ -46,3 +48,28 @@ def test_filter_query_set_exception():
 
     with pytest.raises(ParseError):
         order_by.filter_queryset(request, Authenticator.objects.all(), AuthenticatorViewSet)
+
+
+@pytest.mark.parametrize(
+    "view_ordering,queryset_order_by,expected_order",
+    [
+        (None, None, ('pk',)),
+        ('view', None, ('view',)),
+        ('view', 'category', ('view',)),
+        (None, 'category', ('category',)),
+    ],
+)
+@pytest.mark.django_db
+def test_order_backend_default_order(view_ordering, queryset_order_by, expected_order):
+    class MockView(AnsibleBaseDjangoAppApiView, ModelViewSet):
+        def __init__(self):
+            if view_ordering:
+                self.ordering = view_ordering
+            if queryset_order_by:
+                self.queryset = Authenticator.objects.order_by(queryset_order_by)
+            else:
+                self.queryset = Authenticator.objects.all()
+
+    view = MockView()
+    order_by = OrderByBackend()
+    assert order_by.get_default_ordering(view) == expected_order
