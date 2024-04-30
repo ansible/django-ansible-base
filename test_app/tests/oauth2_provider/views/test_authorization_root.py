7c51b36615a6dced070d02cdbc2e601b7729012d
Rick Elrod <rick@elrod.me>
2024-04-30 21:58:30 +0200

    Tidy up another test file
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- test_app/tests/oauth2_provider/views/test_authorization_root.py
+++ test_app/tests/oauth2_provider/views/test_authorization_root.py
@@ -1,23 +1,12 @@
 from django.urls import reverse
 
 
-def test_oauth2_provider_authorization_root_view_as_admin(admin_api_client):
+def test_oauth2_provider_authorization_root_view(admin_api_client, unauthenticated_api_client, user_api_client):
     """
     As an admin, accessing /o/ gives an index of oauth endpoints.
     """
     url = reverse("oauth_authorization_root_view")
-    response = admin_api_client.get(url)
-
-    assert response.status_code == 200
-    assert 'authorize' in response.data
-
-
-def test_oauth2_provider_authorization_root_view_anon(client):
-    """
-    As an anonymous user, accessing /o/ gives an index of oauth endpoints.
-    """
-    url = reverse("oauth_authorization_root_view")
-    response = client.get(url)
-
-    assert response.status_code == 200
-    assert 'authorize' in response.data
+    for client in (admin_api_client, unauthenticated_api_client, user_api_client):
+        response = admin_api_client.get(url)
+        assert response.status_code == 200
+        assert 'authorize' in response.data
