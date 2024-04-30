a85f912ed1787c032d9afe8708663171cb50e6e6
Rick Elrod <rick@elrod.me>
2024-04-30 02:26:21 +0200

    [activitystream] Nix get_permissions() on RO view
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- ansible_base/activitystream/views.py
+++ ansible_base/activitystream/views.py
@@ -1,13 +1,10 @@
-from django.conf import settings
 from django.utils.translation import gettext_lazy as _
-from rest_framework import permissions
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from ansible_base.activitystream.filtering import ActivityStreamFilterBackend
 from ansible_base.activitystream.models import Entry
 from ansible_base.activitystream.serializers import EntrySerializer
 from ansible_base.lib.utils.views.django_app_api import AnsibleBaseDjangoAppApiView
-from ansible_base.lib.utils.views.permissions import IsSuperuser
 
 
 def calculate_filter_backends():
@@ -41,17 +38,6 @@ class EntryReadOnlyViewSet(ReadOnlyModelViewSet, AnsibleBaseDjangoAppApiView):
     queryset = Entry.objects.order_by('-id')
     serializer_class = EntrySerializer
     filter_backends = calculate_filter_backends()
-
-    def get_permissions(self):
-        """
-        If the RBAC app is enabled, then we delegate to it to manage permissions.
-        Otherwise, we require superuser status.
-        """
-        if 'ansible_base.rbac' in settings.INSTALLED_APPS:
-            permission_classes = [permissions.IsAuthenticated]
-        else:
-            permission_classes = [IsSuperuser]
-        return [permission() for permission in permission_classes]
 
     def get_view_name(self):
         return _('Activity Stream Entries')
