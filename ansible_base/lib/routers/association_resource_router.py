a8d2ccfd4cfc333552de10621cc79ec53a635dfa
AlanCoding <arominge@redhat.com>
2024-05-09 13:52:37 -0400

    Re-organize classes to allow overwriting public methods

--- ansible_base/lib/routers/association_resource_router.py
+++ ansible_base/lib/routers/association_resource_router.py
@@ -4,6 +4,7 @@ from itertools import chain
 from typing import Type
 
 from django.conf import settings
+from django.db.models import Model
 from django.db.models.fields import IntegerField
 from django.db.models.query import QuerySet
 from django.http import QueryDict
@@ -21,7 +22,61 @@ from ansible_base.rbac.permission_registry import permission_registry
 logger = logging.getLogger('ansible_base.lib.routers.association_resource_router')
 
 
+class AssociationViewSetMethodsMixin:
+    """Contains methods called by viewset methods for list, associate, disassociate actions
+
+    Importantly, this is placed high up in the inheritance chain.
+    This means that the viewset passed in for the related views can override these methods.
+    General principal is that these should not overlap with DRF methods, because those
+    are likely already defined by the passed-in viewset and will not take effect.
+    """
+
+    def filter_associate_queryset(self, qs: QuerySet) -> QuerySet:
+        """Limits queryset to objects allowed to be associated
+
+        This is allowed to be different from the filter for the list view,
+        which affects what is visible to the user.
+        Compare to this, which defines what objects can be _associated_
+        probably due to permission restrictions... normally based on visibility.
+        """
+        return self.filter_queryset(qs)
+
+    def get_sublist_queryset(self, parent_instance: Model) -> QuerySet:
+        """Queryset to show, given a parent object
+
+        Example would be /organizations/N/teams/
+        This method returns the team queryset, given organization object pk=N
+        """
+        return getattr(parent_instance, self.association_fk).all()
+
+    def perform_associate(self, parent_instance: Model, related_instances: list[Model]) -> None:
+        """Attach related_instances to instance via the relationship this viewset manages"""
+        manager = getattr(parent_instance, self.association_fk)
+        manager.add(*related_instances)
+
+    def perform_disassociate(self, parent_instance: Model, related_instances: list[Model]) -> None:
+        """Remove related_instances from the managed relationship of instance"""
+        manager = getattr(parent_instance, self.association_fk)
+
+        # Ensure each of the given related_instances is actually related to the instance.
+        # If any isn't, then bomb out and tell the user which ones aren't related.
+        given_related_instance_set = set(related_instances)
+        related_instance_set = set(manager.all())
+        non_related_instances = given_related_instance_set - related_instance_set
+        if non_related_instances:
+            raise serializers.ValidationError(
+                {
+                    'instances': _('Cannot disassociate these objects because they are not all related to this object: %(non_related_instances)s')
+                    % {'non_related_instances': ', '.join(str(i.pk) for i in non_related_instances)},
+                }
+            )
+
+        manager.remove(*related_instances)
+
+
 class RelatedListMixin:
+    """Mixin used for related viewsets which contain a sub-list, like /organizations/N/teams/"""
+
     def check_parent_object_permissions(self, request, parent_obj):
         # Associate and disassociate is a POST request, list is GET
         # the normal process of get_object --> check_object_permissions
@@ -54,7 +109,74 @@ class RelatedListMixin:
 
     def get_queryset(self):
         parent_instance = self.get_parent_object()
-        return getattr(parent_instance, self.association_fk).all()
+        return self.get_sublist_queryset(parent_instance)
+
+
+class AssociateMixin(RelatedListMixin):
+    """Mixin used for writable related viewsets, where objects can be associated or disassociated from the relationship"""
+
+    @action(detail=False, methods=['post'])
+    def associate(self, request, **kwargs):
+        """
+        Associate a related object with this object.
+
+        This will be served at /{basename}/{pk}/{related_name}/associate/
+        We will be given a list of primary keys in the request body.
+        """
+        instance = self.get_parent_object()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        related_instances = serializer.validated_data['instances']
+        if not related_instances:
+            raise serializers.ValidationError({'instances': _('Please pass in one or more instances to associate')})
+
+        self.perform_associate(instance, related_instances)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=False, methods=['post'])
+    def disassociate(self, request, **kwargs):
+        """
+        Disassociate a related object from this object.
+
+        This will be served at /{basename}/{pk}/{related_name}/disassociate/
+        We will be given a list of primary keys in the request body.
+        """
+        instance = self.get_parent_object()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        related_instances = serializer.validated_data['instances']
+        if not related_instances:
+            raise serializers.ValidationError({'instances': _('Please pass in one or more instances to disassociate')})
+
+        self.perform_disassociate(instance, related_instances)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def get_association_queryset(self) -> QuerySet:
+        """Return queryset for instances field of the serializer used for association"""
+        if self.queryset:
+            qs = self.queryset
+            cls = self.queryset.model
+        else:
+            cls = self.serializer_class.Meta.model
+            qs = cls.objects.all()
+
+        if self.action == 'associate':
+            return self.filter_associate_queryset(qs)
+        return qs
+
+    def get_serializer_class(self):
+        if self.action in ('disassociate', 'associate'):
+            rel_name = self.association_fk.replace('_', ' ').title().replace(' ', '')
+            cls_name = f'{self.parent_viewset.__name__}{rel_name}{self.action.title()}Serializer'
+
+            if cls_name not in serializer_registry:
+                qs = self.get_association_queryset()
+                serializer_registry[cls_name] = type(cls_name, (AssociationSerializerBase,), {'target_queryset': qs})
+            return serializer_registry[cls_name]
+
+        return super().get_serializer_class()
 
 
 class AssociationSerializerBase(serializers.Serializer):
@@ -79,89 +201,16 @@ class AssociationSerializerBase(serializers.Serializer):
 serializer_registry = {}
 
 
-class AssociateMixin(RelatedListMixin):
-    @action(detail=False, methods=['post'])
-    def associate(self, request, **kwargs):
-        """
-        Associate a related object with this object.
-
-        This will be served at /{basename}/{pk}/{related_name}/associate/
-        We will be given a list of primary keys in the request body.
-        """
-        instance = self.get_parent_object()
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        related_instances = serializer.validated_data['instances']
-        if not related_instances:
-            raise serializers.ValidationError({'instances': _('Please pass in one or more instances to associate')})
-        manager = getattr(instance, self.association_fk)
-        manager.add(*related_instances)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    @action(detail=False, methods=['post'])
-    def disassociate(self, request, **kwargs):
-        """
-        Disassociate a related object from this object.
-
-        This will be served at /{basename}/{pk}/{related_name}/disassociate/
-        We will be given a list of primary keys in the request body.
-        """
-        instance = self.get_parent_object()
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        related_instances = serializer.validated_data['instances']
-        if not related_instances:
-            raise serializers.ValidationError({'instances': _('Please pass in one or more instances to disassociate')})
-        manager = getattr(instance, self.association_fk)
-
-        # Ensure each of the given related_instances is actually related to the instance.
-        # If any isn't, then bomb out and tell the user which ones aren't related.
-        given_related_instance_set = set(related_instances)
-        related_instance_set = set(manager.all())
-        non_related_instances = given_related_instance_set - related_instance_set
-        if non_related_instances:
-            raise serializers.ValidationError(
-                {
-                    'instances': _('Cannot disassociate these objects because they are not all related to this object: %(non_related_instances)s')
-                    % {'non_related_instances': ', '.join(str(i.pk) for i in non_related_instances)},
-                }
-            )
-
-        manager.remove(*related_instances)
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def get_association_queryset(self) -> QuerySet:
-        if self.queryset:
-            qs = self.queryset
-            cls = self.queryset.model
-        else:
-            cls = self.serializer_class.Meta.model
-            qs = cls.objects.all()
-
-        if self.action == 'associate':
-            if hasattr(self, 'filter_associate_queryset'):
-                return self.filter_associate_queryset(qs)
-            else:
-                return self.filter_queryset(qs)
-        return qs
-
-    def get_serializer_class(self):
-        if self.action in ('disassociate', 'associate'):
-            qs = self.get_association_queryset()
-
-            rel_name = self.association_fk.replace('_', ' ').title().replace(' ', '')
-            cls_name = f'{self.parent_viewset.__name__}{rel_name}AssociationSerializer'
-
-            if cls_name not in serializer_registry:
-                serializer_registry[cls_name] = type(cls_name, (AssociationSerializerBase,), {'target_queryset': qs})
-            return serializer_registry[cls_name]
-
-        return super().get_serializer_class()
-
-
 @property
 def attribute_raiser(cls):
+    """This method is used to exclude methods in subclass of provided viewset
+
+    How it works:
+    SimpleRouter.get_method_map determines what actions are in a viewset with hasattr.
+    raising AttributeError makes that return False, so methods like retrieve
+    will not be included in the returned method map, so it will not build a URL
+    for those, because we do not want them in the association URL set.
+    """
     raise AttributeError
 
 
@@ -190,7 +239,7 @@ class AssociationResourceRouter(routers.SimpleRouter):
         for method in ('retrieve', 'update', 'partial_update', 'destroy') + tuple(custom_methods):
             setattr(AssociatedViewSetType, method, attribute_raiser)
 
-        class AssociatedViewSet(viewset, metaclass=AssociatedViewSetType):
+        class AssociatedViewSet(viewset, AssociationViewSetMethodsMixin, metaclass=AssociatedViewSetType):
             """Adjusted version of given viewset for related endpoint with only list views"""
 
             pass
