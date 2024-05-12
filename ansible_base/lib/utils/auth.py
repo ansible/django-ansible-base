a94809474ff8eec6013c6f7fdd1fdaa6192bbcb2
James Tanner <tanner.jc@gmail.com>
2024-05-12 17:25:08 -0400

    More type hinting.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- ansible_base/lib/utils/auth.py
+++ ansible_base/lib/utils/auth.py
@@ -1,8 +1,11 @@
-from typing import Any
+from typing import Any, Type
 
 from django.apps import apps as django_apps
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
+
+from ansible_base.lib.abstract_models.organization import AbstractOrganization
+from ansible_base.lib.abstract_models.team import AbstractTeam
 
 
 def get_model_from_settings(setting_name: str) -> Any:
@@ -21,9 +24,9 @@ def get_model_from_settings(setting_name: str) -> Any:
         raise ImproperlyConfigured(f"{setting_name} refers to model '{setting}' that has not been installed")
 
 
-def get_team_model():
+def get_team_model() -> Type[AbstractTeam]:
     return get_model_from_settings('ANSIBLE_BASE_TEAM_MODEL')
 
 
-def get_organization_model():
+def get_organization_model() -> Type[AbstractOrganization]:
     return get_model_from_settings('ANSIBLE_BASE_ORGANIZATION_MODEL')
