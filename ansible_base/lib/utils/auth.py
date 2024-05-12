d458c1c6a5248b1c6d90dd41aa9bce8adf85688b
James Tanner <tanner.jc@gmail.com>
2024-05-12 17:19:48 -0400

    Catch attrib error.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- ansible_base/lib/utils/auth.py
+++ ansible_base/lib/utils/auth.py
@@ -7,7 +7,10 @@ def get_model_from_settings(setting_name):
     """
     Return the User model that is active in this project.
     """
-    setting = getattr(settings, setting_name)
+    try:
+        setting = getattr(settings, setting_name)
+    except AttributeError:
+        raise ImproperlyConfigured(f"{setting_name} is not defined in settings.")
     try:
         return django_apps.get_model(setting, require_ready=False)
     except ValueError:
