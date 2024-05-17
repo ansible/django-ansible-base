0dc482eafef120cf25c18b22e74ab4f026f3527b
Bryan Havenstein <bhavenst@redhat.com>
2024-05-17 11:52:30 -0400

    Linter

--- ansible_base/authentication/authenticator_plugins/keycloak.py
+++ ansible_base/authentication/authenticator_plugins/keycloak.py
@@ -52,4 +52,5 @@ class AuthenticatorPlugin(SocialAuthMixin, KeycloakOAuth2, AbstractAuthenticator
     def extra_data(self, user, uid, response, details=None, *args, **kwargs):
         data = super().extra_data(user, uid, response, details=None, *args, **kwargs)
         for perm in ["is_superuser", "is_system_auditor"]:
-            if perm in response and bool(response[perm]): data[perm] = response[perm]
+            if perm in response and bool(response[perm]):
+                data[perm] = response[perm]
--- ansible_base/authentication/authenticator_plugins/oidc.py
+++ ansible_base/authentication/authenticator_plugins/oidc.py
@@ -209,4 +209,5 @@ class AuthenticatorPlugin(SocialAuthMixin, OpenIdConnectAuth, AbstractAuthentica
     def extra_data(self, user, uid, response, details=None, *args, **kwargs):
         data = super().extra_data(user, uid, response, details=None, *args, **kwargs)
         for perm in ["is_superuser", "is_system_auditor"]:
-            if perm in response and bool(response[perm]): data[perm] = response[perm]
+            if perm in response and bool(response[perm]):
+                data[perm] = response[perm]
