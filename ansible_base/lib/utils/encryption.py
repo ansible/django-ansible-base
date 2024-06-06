e851bbd6dd074546da1349853124b48bc213f947
Truc Duong <tduong@tduong-na.remote.csb>
2024-06-06 07:01:54 -0400

    Fix issues with saving secrets starting with $encrypted$

--- ansible_base/lib/utils/encryption.py
+++ ansible_base/lib/utils/encryption.py
@@ -4,6 +4,7 @@ import hmac
 import json
 import logging
 import time
+import re
 from typing import Optional
 
 from cryptography.fernet import Fernet
@@ -43,34 +44,69 @@ class Fernet256(Fernet):
         self._encryption_key = self.key[32:]
         self._backend = default_backend()
 
-    def encrypt_string(self, value: str) -> str:
-        # Its possible for a serializer to accept a number for a CharField (like 5). In the serializer its "5" but when we get here it might be 5
-        if type(value) is not str:
-            value = str(value)
-
-        if value.startswith(ENCRYPTED_STRING):
-            return value
-        encrypted = self.encrypt(smart_bytes(value))
-        b64data = smart_str(base64.b64encode(encrypted))
-        return f'{ENCRYPTED_STRING}UTF8${ENCRYPTION_METHOD}${b64data}'
-
-    def decrypt_string(self, value: str) -> str:
-        if type(value) is not str:
-            raise ValueError("decrypt_string can only accept string")
-
-        if not value.startswith(ENCRYPTED_STRING):
-            return value
-
-        raw_data = value[len(ENCRYPTED_STRING) :]
+    def extract_data(self, raw_data: str) -> str:
+        # Sequential data extract for code reuse in is_encrypted_string and decrypt_string
+        raw_data = raw_data[len(ENCRYPTED_STRING) :]
 
         # If the encrypted string contains a UTF8 marker, discard it
         if raw_data.startswith('UTF8$'):
             raw_data = raw_data[len('UTF8$') :]
 
         # Extract the algorithm and ensure its what we expect
-        algo, b64data = raw_data.split('$', 1)
+        algo, data = raw_data.split('$', 1)
         if algo != ENCRYPTION_METHOD:
             raise ValueError(f'Unsupported algorithm: {algo}')
+
+        return data
+
+    def is_encrypted_string(self, value: str, invalid_algo_is_fatal: bool) -> bool:
+        # Ensure input is a string already encrypted by our algorithm
+        # by comparing with the decrypted and re-encrypted values
+
+        if type(value) is not str:
+            raise ValueError("is_encrypted_string can only accept string")
+
+        if not value.startswith(ENCRYPTED_STRING):
+            return False
+
+        try:
+            data = self.extract_data(value)
+        except ValueError as ve:
+            if invalid_algo_is_fatal:
+                raise ve
+
+            print('Data is not in the correct format')
+            return False
+
+        # Check if data is base64 encoded
+        # Note: this can fail because some strings resemble base 64 encoded ones. **important**
+        # first check: if length is multiple of 4
+        if len(data.strip()) % 4 != 0:
+            return False
+
+        # second check: use regex pattern
+        reg = r'^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$'
+
+        return re.match(reg, data) is not None
+
+    def encrypt_string(self, value: str) -> str:
+
+        # Its possible for a serializer to accept a number for a CharField (like 5). In the serializer its "5" but when we get here it might be 5
+        if type(value) is not str:
+            value = str(value)
+
+        if self.is_encrypted_string(value, invalid_algo_is_fatal=False):
+            return value
+
+        encrypted = self.encrypt(smart_bytes(value))
+        b64data = smart_str(base64.b64encode(encrypted))
+        return f'{ENCRYPTED_STRING}UTF8${ENCRYPTION_METHOD}${b64data}'
+
+    def decrypt_string(self, value: str) -> str:
+        if not self.is_encrypted_string(value, invalid_algo_is_fatal=True):
+            return value
+
+        b64data = self.extract_data(value)
 
         # Finally decode the value
         encrypted = base64.b64decode(b64data)
