3710587404018eb02000ab17b9d86b8f16f65e54
AlanCoding <arominge@redhat.com>
2024-06-13 14:44:20 -0400

    Flake8

--- ansible_base/lib/utils/auth.py
+++ ansible_base/lib/utils/auth.py
@@ -37,7 +37,7 @@ def get_organization_model() -> Type[AbstractOrganization]:
     return get_model_from_settings('ANSIBLE_BASE_ORGANIZATION_MODEL')
 
 
-def get_object_by_ansible_id(qs: QuerySet, ansible_id: Union[str, UUID], annotate_as: str='ansible_id_for_filter') -> Model:
+def get_object_by_ansible_id(qs: QuerySet, ansible_id: Union[str, UUID], annotate_as: str = 'ansible_id_for_filter') -> Model:
     resource_cls = django_apps.get_model('dab_resource_registry', 'Resource')
     content_type_cls = django_apps.get_model('contenttypes', 'ContentType')
     cls = qs.model
@@ -48,5 +48,5 @@ def get_object_by_ansible_id(qs: QuerySet, ansible_id: Union[str, UUID], annotat
     return qs.annotate(**{annotate_as: resource_qs}).get(**{annotate_as: ansible_id})
 
 
-def get_user_by_ansible_id(ansible_id: Union[str, UUID], annotate_as: str='ansible_id_for_filter') -> Model:
+def get_user_by_ansible_id(ansible_id: Union[str, UUID], annotate_as: str = 'ansible_id_for_filter') -> Model:
     return get_object_by_ansible_id(get_user_model().objects.all(), ansible_id, annotate_as=annotate_as)
