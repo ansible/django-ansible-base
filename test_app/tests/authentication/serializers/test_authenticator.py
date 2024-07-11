7595830ef70814a31e43fc5ab5f03c5ac0fefedc
James Tanner <tanner.jc@gmail.com>
2024-07-11 15:06:53 -0400

    Add a unit test.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- test_app/tests/authentication/serializers/test_authenticator.py
+++ test_app/tests/authentication/serializers/test_authenticator.py
@@ -4,6 +4,7 @@ import pytest
 from rest_framework.serializers import ValidationError
 
 from ansible_base.authentication.serializers import AuthenticatorSerializer
+from ansible_base.authentication.models import Authenticator
 
 
 @pytest.mark.django_db
@@ -61,3 +62,33 @@ def test_authenticator_validate_import_error(shut_up_logging):
                     "order": 497,
                 }
             )
+
+# system_user, admin_user, live_server, local_authenticator, transactional_db
+@pytest.mark.django_db
+def test_disable_last_enabled_authenticator(system_user, local_authenticator):
+    # Instantiate a serializer
+    serializer = AuthenticatorSerializer()
+
+    # the serializer won't have an object instance to check
+    # for the current state of the enabled field unless we
+    # manually set it here ...
+    serializer.instance = local_authenticator
+
+    # we have mock out the request method so that it doesn't bomb out on an empty config
+    with (
+        mock.patch(
+            "ansible_base.authentication.serializers.authenticator.AuthenticatorSerializer.context",
+            return_value={'request': {'method': 'PATCH'}},
+        )
+    ):
+        # validation should fail ...
+        with pytest.raises(ValidationError) as exc_info:
+            serializer.validate({
+                'id': local_authenticator.id,
+                'type': local_authenticator.type,
+                'name': local_authenticator.name,
+                'enabled': False,
+                'configuration': {},
+            })
+    
+    assert "At least one authenticator must be enabled" in str(exc_info.value)
