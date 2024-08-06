3a6d8e627294ad2b8c580fdf2bfbad4f0d4701ee
Alan Rominger <arominge@redhat.com>
2024-08-06 12:58:27 -0400

    Do not run resource registry stuff if migrations are not finished
    Move into utils method and test

--- ansible_base/lib/utils/db.py
+++ ansible_base/lib/utils/db.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 
-from django.db import transaction
+from django.db import connection, transaction
+from django.db.migrations.executor import MigrationExecutor
 
 
 @contextmanager
@@ -12,3 +13,15 @@ def ensure_transaction():
             yield
     else:
         yield
+
+
+def migrations_are_complete() -> bool:
+    """Returns a booling telling you if manage.py migrate has been run to completion
+
+    Note that this is a little expensive, like up to 20 database queries
+    and lots of imports.
+    Not suitable to run as part of a request, but expected to be okay
+    in a management command or post_migrate signals"""
+    executor = MigrationExecutor(connection)
+    plan = executor.migration_plan(executor.loader.graph.leaf_nodes())
+    return not bool(plan)
