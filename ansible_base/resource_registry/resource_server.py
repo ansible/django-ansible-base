27f2a13b491018f16ed0542534b83d6467dba21c
David Newswanger <dnewswan@redhat.com>
2024-08-09 10:22:50 -0600

    Add resource server social auth strategy

--- ansible_base/resource_registry/resource_server.py
+++ ansible_base/resource_registry/resource_server.py
@@ -4,7 +4,7 @@ from typing import TypedDict
 import jwt
 from django.conf import settings
 
-from ansible_base.resource_registry.models import service_id
+from ansible_base.resource_registry.models import Resource, service_id
 
 
 class ResourceServerConfig(TypedDict):
@@ -32,5 +32,27 @@ def get_service_token(user_id=None, expiration=60, **kwargs):
 
     if expiration is not None:
         payload["exp"] = datetime.now() + timedelta(seconds=expiration)
+
+    return jwt.encode(payload, config["SECRET_KEY"], config["JWT_ALGORITHM"])
+
+
+def get_user_auth_code(user):
+    """
+    Generate an authentication code using the service's configured secret key.
+    """
+    config = get_resource_server_config()
+    payload = {
+        "iss": str(service_id()),
+        "type": "auth_code",
+        "username": user.username,
+        "sub": str(Resource.get_resource_for_object(user).ansible_id),
+        "exp": datetime.now() + timedelta(seconds=15),
+        "sso_uid": None,
+        "sso_backend": None,
+    }
+
+    if hasattr(user, "social_user"):
+        payload["sso_uid"] = user.social_user.uid
+        payload["sso_backend"] = user.social_user.provider
 
     return jwt.encode(payload, config["SECRET_KEY"], config["JWT_ALGORITHM"])
