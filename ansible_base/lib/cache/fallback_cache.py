339c61f04beffa02a8636434371a2eb375d3d5aa
Bryan Havenstein <bhavenst@redhat.com>
2024-08-20 09:55:37 -0600

    Switch to thread

--- ansible_base/lib/cache/fallback_cache.py
+++ ansible_base/lib/cache/fallback_cache.py
@@ -1,7 +1,8 @@
-import asyncio
 import logging
 import multiprocessing
+import time
 
+from threading import Thread
 from django.core import cache as django_cache
 from django.core.cache.backends.base import BaseCache
 
@@ -71,20 +72,29 @@ class DABCacheWithFallback(BaseCache):
                 with multiprocessing.Lock():
                     if not self._status_cache.get(RECOVERY_KEY, default=False):
                         logger.error("Primary cache unavailable, switching to fallback cache.")
-                        self._status_cache.set(CACHE_STATUS_KEY, FALLBACK_CACHE)
                         self._status_cache.set(RECOVERY_KEY, True)
-                        asyncio.run(self._recover_primary())
+                        self._status_cache.set(CACHE_STATUS_KEY, FALLBACK_CACHE)
+                        RecoveryThread(self._primary_cache, self._status_cache, self._recovery_check_freq_sec).start()
         response = getattr(self._fallback_cache, operation)(*args, **kwargs)
         return response
 
-    async def _recover_primary(self):
-        await asyncio.sleep(self._recovery_check_freq_sec)
-        try:
-            self._primary_cache.get('fakekey')
-            logger.warning("Primary cache recovered, clearing and resuming use.")
-            self._primary_cache.clear()
-            self._status_cache.set(CACHE_STATUS_KEY, PRIMARY_CACHE)
-            self._status_cache.set(RECOVERY_KEY, False)
-        except Exception:
-            logger.error("Primary cache still not available, retrying.")
-            await self._recover_primary()
+
+class RecoveryThread(Thread):
+    def __init__(self, primary_cache, status_cache, retry_seconds):
+        Thread.__init__(self)
+        self.primary_cache = primary_cache
+        self.status_cache = status_cache
+        self.retry_seconds = retry_seconds
+
+    def run(self):
+        while True:
+            time.sleep(self.retry_seconds)
+            try:
+                self.primary_cache.get('317783e3-03f6-4f05-9a22-7c774a0bbea3', default=True)
+                logger.warning(f"Primary cache recovered, clearing and resuming use. {self}")
+                self.primary_cache.clear()
+                self.status_cache.set(CACHE_STATUS_KEY, PRIMARY_CACHE)
+                self.status_cache.set(RECOVERY_KEY, False)
+                return
+            except Exception as e:
+                logger.error(f"Primary cache still not available, {self} retrying: {e}")
