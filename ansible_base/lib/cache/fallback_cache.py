5b4b415198456ef379eeca35d5f09d5d3dc83109
Bryan Havenstein <bhavenst@redhat.com>
2024-08-23 11:22:36 -0600

    Ensure single thread recovery

--- ansible_base/lib/cache/fallback_cache.py
+++ ansible_base/lib/cache/fallback_cache.py
@@ -1,6 +1,8 @@
 import logging
 import multiprocessing
+import random
 import tempfile
+import time
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 
@@ -63,13 +65,16 @@ class DABCacheWithFallback(BaseCache):
     def _op_with_fallback(self, operation, *args, **kwargs):
         if _temp_file.exists():
             response = getattr(self._fallback_cache, operation)(*args, **kwargs)
-            self.thread_pool.submit(DABCacheWithFallback.check_primary_cache)
+            self.start_recovery()
         else:
             try:
                 response = getattr(self._primary_cache, operation)(*args, **kwargs)
                 return response
             except Exception:
                 with multiprocessing.Lock():
+                    # Attempt to ensure one thread/process goes first
+                    # dynamic settings especially are read in a batch very quickly
+                    time.sleep(random.uniform(10, 100) / 100.0)
                     if not _temp_file.exists():
                         logger.error("Primary cache unavailable, switching to fallback cache.")
                     _temp_file.touch()
@@ -77,19 +82,28 @@ class DABCacheWithFallback(BaseCache):
 
         return response
 
-    @staticmethod
-    def check_primary_cache():
+    def start_recovery(self):
+        with multiprocessing.Lock():
+            # Set single process/thread to do the recovery, but time out in case it dies
+            recoverer = self._fallback_cache.get_or_set('RECOVERY_THREAD_ID', id(self), timeout=60)
+            if recoverer == id(self):
+                rip = self._fallback_cache.get('RECOVERY_IN_PROGRESS', False)
+                if not rip:
+                    self._fallback_cache.set('RECOVERY_IN_PROGRESS', True, timeout=60)
+                    self.thread_pool.submit(self.check_primary_cache)
+
+    def check_primary_cache(self):
         try:
-            primary_cache = django_cache.caches.create_connection(PRIMARY_CACHE)
-            primary_cache.get('up_test')
+            self._primary_cache.get('up_test')
             with multiprocessing.Lock():
                 if _temp_file.exists():
                     logger.warning("Primary cache recovered, clearing and resuming use.")
                     # Clear the primary cache
-                    primary_cache.clear()
+                    self._primary_cache.clear()
                     # Clear the backup cache just incase we need to fall back again (don't want it out of sync)
-                    fallback_cache = django_cache.caches.create_connection(FALLBACK_CACHE)
-                    fallback_cache.clear()
+                    self._fallback_cache.clear()
                     _temp_file.unlink()
         except Exception:
             pass
+        finally:
+            self._fallback_cache.delete('RECOVERY_IN_PROGRESS')
