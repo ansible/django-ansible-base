067107be8b1cdd3198c4231effd2c70311055af9
Rick Elrod <rick@elrod.me>
2024-08-23 16:01:20 +0200

    Attempt to lock down dependencies
    In the current world, we do not lock down dependencies at all. This
    means that developers are all potentially developing against moving
    targets, and some dependencies from which we subclass models could
    generate migrations (which could be backwards incompatible and cause
    issues for environments that use system packages pinned to older
    versions).
    
    In this commit is really 3 changes:
    
    - Update the updater script a bit - let it write out all the
      DAB application .txt files
    - Make pyproject.toml use those .txt files
    - Add current working/known upper bounds to all deps where possible
      before generating the .txt files.
    
    I believe this is the first step towards sanity here and vision into
    which versions of dependencies are getting pulled in and where.
    
    In the current world, we have a dep that released a new z-stream
    release which changed a field in a baseclass we're using. For
    developers who got that new version of the library (since we don't pin
    deps currently), it is easy to think that we need to push the new
    migration that gets generated. However we have no idea currently if
    that change would be compatible with apps using DAB, or apps forced to
    use the old version of the dependency due to system packages.
    
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- requirements/updater.sh
+++ requirements/updater.sh
@@ -1,62 +1,69 @@
 #!/usr/bin/env bash
+# -*- coding: utf-8; mode: sh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+
 set -ue
 
 PYTHON=python3.11
 
 for FILE in requirements.in requirements_all.txt ; do
-	if [ ! -f ${FILE} ] ; then
-		touch ${FILE}
-	fi
+  if [ ! -f ${FILE} ] ; then
+    touch ${FILE}
+  fi
 done
 requirements_dir="$(readlink -f .)"
 requirements_txt="$(readlink -f ./requirements_all.txt)"
 pip_compile="pip-compile --no-header --quiet -r --allow-unsafe"
 
 _cleanup() {
-  cd /
   test "${KEEP_TMP:-0}" = 1 || rm -rf "${_tmp}"
 }
 
 generate_requirements() {
-  venv="`pwd`/venv"
-  echo $venv
+  venv="$(pwd)/venv"
+  echo "Using virtual environment: $venv"
   ${PYTHON} -m venv "${venv}"
   # shellcheck disable=SC1090
   source ${venv}/bin/activate
 
-  ${venv}/bin/python -m pip install -U 'pip' pip-tools
+  ${venv}/bin/pip install -U pip pip-tools
 
-  ${pip_compile} $(ls ${requirements_dir}/requirements*.in | xargs) --output-file requirements_all.txt
+  ${pip_compile} ${requirements_dir}/requirements*.in --output-file requirements_all.txt
+
+  for file in ${requirements_dir}/requirements*.in; do
+    app="$(basename $file .in)"
+    echo "Compiling deps for $app"
+    ${pip_compile} $file --output-file "$app.txt"
+  done
 }
 
 main() {
   base_dir=$(pwd)
 
-  _tmp=$(${PYTHON} -c "import tempfile; print(tempfile.mkdtemp(suffix='.ansible_base-requirements', dir='/tmp'))")
+  _tmp=$(mktemp -d /tmp/tmp.XXXXXX.ansible_base-requirements)
 
   trap _cleanup INT TERM EXIT
 
   case $1 in
     "run")
       NEEDS_HELP=0
-    ;;
+      ;;
     "upgrade")
       NEEDS_HELP=0
       pip_compile="${pip_compile} --upgrade"
-    ;;
+      ;;
     "help")
       NEEDS_HELP=1
-    ;;
+      ;;
     *)
       echo ""
       echo "ERROR: Parameter $1 not valid"
       echo ""
       NEEDS_HELP=1
-    ;;
-  esac 
+      ;;
+  esac
 
   if [[ "$NEEDS_HELP" == "1" ]] ; then
-    echo "This script generates requirements_all.txt from requirements[_*].in"
+    echo "This script generates pinned requirements.txt files from requirements[_*].in"
     echo ""
     echo "Usage: $0 [run|upgrade]"
     echo ""
@@ -68,17 +75,16 @@ main() {
     exit
   fi
 
-  cp -vf ${requirements_txt} "${_tmp}"
   cd "${_tmp}"
-
   generate_requirements
 
   echo "Changing $base_dir to requirements"
-  cat requirements_all.txt | sed "s:$base_dir:requirements:" > "${requirements_txt}"
+  for file in requirements*.txt; do
+    sed "s:$base_dir:requirements:" "$file" > "$requirements_dir/$file"
+  done
 
   _cleanup
 }
 
 # set EVAL=1 in case you want to source this script
 test "${EVAL:-0}" -eq "1" || main "${1:-}"
-
