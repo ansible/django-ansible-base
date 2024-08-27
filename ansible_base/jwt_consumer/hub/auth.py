c342c16bdbf0a2b5ee942d00ac88f80509462069
jctanner <tanner.jc@gmail.com>
2024-08-27 17:54:14 +0200

    Test.
    No-Issue
    
    Signed-off-by: jctanner <tanner.jc@gmail.com>

--- ansible_base/jwt_consumer/hub/auth.py
+++ ansible_base/jwt_consumer/hub/auth.py
@@ -5,6 +5,8 @@ from django.contrib.contenttypes.models import ContentType
 from ansible_base.jwt_consumer.common.auth import JWTAuthentication
 from ansible_base.jwt_consumer.common.exceptions import InvalidService
 from ansible_base.resource_registry.models import Resource
+from ansible_base.rbac.models import RoleDefinition, RoleUserAssignment
+
 
 logger = logging.getLogger('ansible_base.jwt_consumer.hub.auth')
 
@@ -27,8 +29,10 @@ class HubJWTAuth(JWTAuthentication):
         self.team_content_type = ContentType.objects.get_for_model(Team)
         self.org_content_type = ContentType.objects.get_for_model(Organization)
 
-        orgs = []
-        teams = []
+        admin_orgs = []
+        member_orgs = []
+        admin_teams = []
+        member_teams = []
         groups = []
         for role_name in self.common_auth.token.get('object_roles', {}).keys():
             if role_name.startswith('Org'):
@@ -41,7 +45,10 @@ class HubJWTAuth(JWTAuthentication):
                     except Resource.DoesNotExist:
                         org = self.common_auth.get_or_create_resource('organization', org_data)[1]
 
-                    orgs.append(org)
+                if role_name == 'Organization Admin':
+                    admin_orgs.append(org)
+                else:
+                    member_orgs.append(org)
 
             if role_name.startswith('Team'):
                 for object_index in self.common_auth.token['object_roles'][role_name]['objects']:
@@ -51,25 +58,37 @@ class HubJWTAuth(JWTAuthentication):
                         team = Resource.objects.get(ansible_id=ansible_id).content_object
                     except Resource.DoesNotExist:
                         team = self.common_auth.get_or_create_resource('team', team_data)[1]
-                    teams.append(team)
 
-                    groups.append(team.group)
+                groups.append(team.group)
+
+                if role_name == 'Team Member':
+                    member_teams.append(team)
 
         self.common_auth.user.groups.set(groups)
 
+        '''
         # manage org membership ...
         org_pks = [org.pk for org in orgs]
         for org in Organization.objects.exclude(pk__in=org_pks).filter(users=self.common_auth.user):
             org.users.remove(self.common_auth.user)
         for org in orgs:
             org.users.add(self.common_auth.user)
+        '''
 
         # manage team membership ...
-        team_pks = [team.pk for team in teams]
-        for team in Team.objects.exclude(pk__in=team_pks).filter(users=self.common_auth.user):
-            team.users.remove(self.common_auth.user)
-        for team in teams:
-            team.users.add(self.common_auth.user)
+        member_team_pks = [team.pk for team in member_teams]
+        # delete all memberships not defined by this jwt ...
+        RoleUserAssignment.objects.filter(
+            user=self.common_auth.user
+        ).exclude(object_id__in=member_team_pks).delete()
+        # assign "local" membership for each team ...
+        roledef = RoleDefinition.objects.get(name='Galaxy Team Member')
+        for team in member_teams:
+            RoleUserAssignment.objects.get_or_create(
+                user=self.common_auth.user,
+                role_definition=roledef,
+                object_id=team.pk
+            )
 
         if "Platform Auditor" in self.common_auth.token.get('global_roles', []):
             assign_role("galaxy.auditor", self.common_auth.user)
