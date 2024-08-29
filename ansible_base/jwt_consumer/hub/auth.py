fa97197950ee806659d61e7591183fa391468d00
jctanner <tanner.jc@gmail.com>
2024-08-29 09:40:28 -0400

    HUB is removing org.users and team.users (#591)
    Org.users and Team.users were not a pattern we wanted to keep long term. This patch establishes
    memberships via role assignments instead and uses the appropriate helper functions.
    
    related https://github.com/ansible/galaxy_ng/pull/2248
    
    Signed-off-by: jctanner <tanner.jc@gmail.com>

--- ansible_base/jwt_consumer/hub/auth.py
+++ ansible_base/jwt_consumer/hub/auth.py
@@ -4,6 +4,7 @@ from django.contrib.contenttypes.models import ContentType
 
 from ansible_base.jwt_consumer.common.auth import JWTAuthentication
 from ansible_base.jwt_consumer.common.exceptions import InvalidService
+from ansible_base.rbac.models import RoleDefinition, RoleUserAssignment
 from ansible_base.resource_registry.models import Resource
 
 logger = logging.getLogger('ansible_base.jwt_consumer.hub.auth')
@@ -11,38 +12,34 @@ logger = logging.getLogger('ansible_base.jwt_consumer.hub.auth')
 
 class HubJWTAuth(JWTAuthentication):
 
-    def get_galaxy_models_and_functions(self):
+    def get_galaxy_models(self):
         '''This is separate from process_permissions purely for testability.'''
         try:
             from galaxy_ng.app.models import Organization, Team
-            from pulpcore.plugin.util import assign_role, remove_role
         except ImportError:
             raise InvalidService("automation-hub")
 
-        return Organization, Team, assign_role, remove_role
+        return Organization, Team
 
     def process_permissions(self):
         # Map teams in the JWT to Automation Hub groups.
-        Organization, Team, assign_role, remove_role = self.get_galaxy_models_and_functions()
+        Organization, Team = self.get_galaxy_models()
         self.team_content_type = ContentType.objects.get_for_model(Team)
         self.org_content_type = ContentType.objects.get_for_model(Organization)
 
-        orgs = []
-        teams = []
-        groups = []
+        # TODO - galaxy does not have an org admin roledef yet
+        # admin_orgs = []
+
+        # TODO - galaxy does not have an org member roledef yet
+        # member_orgs = []
+
+        # The "shared" [!local] teams this user admins
+        admin_teams = []
+
+        # the teams this user should have a "shared" [!local] assignment to
+        member_teams = []
+
         for role_name in self.common_auth.token.get('object_roles', {}).keys():
-            if role_name.startswith('Org'):
-                for object_index in self.common_auth.token['object_roles'][role_name]['objects']:
-                    org_data = self.common_auth.token['objects']['organization'][object_index]
-                    ansible_id = org_data['ansible_id']
-
-                    try:
-                        org = Resource.objects.get(ansible_id=ansible_id).content_object
-                    except Resource.DoesNotExist:
-                        org = self.common_auth.get_or_create_resource('organization', org_data)[1]
-
-                    orgs.append(org)
-
             if role_name.startswith('Team'):
                 for object_index in self.common_auth.token['object_roles'][role_name]['objects']:
                     team_data = self.common_auth.token['objects']['team'][object_index]
@@ -51,27 +48,31 @@ class HubJWTAuth(JWTAuthentication):
                         team = Resource.objects.get(ansible_id=ansible_id).content_object
                     except Resource.DoesNotExist:
                         team = self.common_auth.get_or_create_resource('team', team_data)[1]
-                    teams.append(team)
 
-                    groups.append(team.group)
+                    if role_name == 'Team Admin':
+                        admin_teams.append(team)
+                    elif role_name == 'Team Member':
+                        member_teams.append(team)
 
-        self.common_auth.user.groups.set(groups)
+        for roledef_name, teams in [('Team Admin', admin_teams), ('Team Member', member_teams)]:
 
-        # manage org membership ...
-        org_pks = [org.pk for org in orgs]
-        for org in Organization.objects.exclude(pk__in=org_pks).filter(users=self.common_auth.user):
-            org.users.remove(self.common_auth.user)
-        for org in orgs:
-            org.users.add(self.common_auth.user)
+            # the "shared" "non-local" definition ...
+            roledef = RoleDefinition.objects.get(name=roledef_name)
 
-        # manage team membership ...
-        team_pks = [team.pk for team in teams]
-        for team in Team.objects.exclude(pk__in=team_pks).filter(users=self.common_auth.user):
-            team.users.remove(self.common_auth.user)
-        for team in teams:
-            team.users.add(self.common_auth.user)
+            # pks for filtering ...
+            team_pks = [team.pk for team in teams]
 
+            # delete all assignments not defined by this jwt ...
+            for assignment in RoleUserAssignment.objects.filter(user=self.common_auth.user, role_definition=roledef).exclude(object_id__in=team_pks):
+                team = Team.objects.get(pk=assignment.object_id)
+                roledef.remove_permission(self.common_auth.user, team)
+
+            # assign "non-local" for each team ...
+            for team in teams:
+                roledef.give_permission(self.common_auth.user, team)
+
+        auditor_roledef = RoleDefinition.objects.get(name='Platform Auditor')
         if "Platform Auditor" in self.common_auth.token.get('global_roles', []):
-            assign_role("galaxy.auditor", self.common_auth.user)
+            auditor_roledef.give_global_permission(self.common_auth.user)
         else:
-            remove_role("galaxy.auditor", self.common_auth.user)
+            auditor_roledef.remove_global_permission(self.common_auth.user)
