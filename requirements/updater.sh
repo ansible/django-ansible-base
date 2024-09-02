dc7af6a4cc3302bfb4180a8999a8851a9aa221c9
Rick Elrod <rick@elrod.me>
2024-09-02 15:01:14 +0200

    Revert "Attempt to lock down dependencies"
    This reverts commit 067107be8b1cdd3198c4231effd2c70311055af9.

--- requirements/updater.sh
+++ requirements/updater.sh
@@ -1,69 +1,62 @@
 #!/usr/bin/env bash
-# -*- coding: utf-8; mode: sh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
-
 set -ue
 
 PYTHON=python3.11
 
 for FILE in requirements.in requirements_all.txt ; do
-  if [ ! -f ${FILE} ] ; then
-    touch ${FILE}
-  fi
+	if [ ! -f ${FILE} ] ; then
+		touch ${FILE}
+	fi
 done
 requirements_dir="$(readlink -f .)"
 requirements_txt="$(readlink -f ./requirements_all.txt)"
 pip_compile="pip-compile --no-header --quiet -r --allow-unsafe"
 
 _cleanup() {
+  cd /
   test "${KEEP_TMP:-0}" = 1 || rm -rf "${_tmp}"
 }
 
 generate_requirements() {
-  venv="$(pwd)/venv"
-  echo "Using virtual environment: $venv"
+  venv="`pwd`/venv"
+  echo $venv
   ${PYTHON} -m venv "${venv}"
   # shellcheck disable=SC1090
   source ${venv}/bin/activate
 
-  ${venv}/bin/pip install -U pip pip-tools
+  ${venv}/bin/python -m pip install -U 'pip' pip-tools
 
-  ${pip_compile} ${requirements_dir}/requirements*.in --output-file requirements_all.txt
-
-  for file in ${requirements_dir}/requirements*.in; do
-    app="$(basename $file .in)"
-    echo "Compiling deps for $app"
-    ${pip_compile} $file --output-file "$app.txt"
-  done
+  ${pip_compile} $(ls ${requirements_dir}/requirements*.in | xargs) --output-file requirements_all.txt
 }
 
 main() {
   base_dir=$(pwd)
 
-  _tmp=$(mktemp -d /tmp/tmp.XXXXXX.ansible_base-requirements)
+  _tmp=$(${PYTHON} -c "import tempfile; print(tempfile.mkdtemp(suffix='.ansible_base-requirements', dir='/tmp'))")
 
   trap _cleanup INT TERM EXIT
 
   case $1 in
     "run")
       NEEDS_HELP=0
-      ;;
+    ;;
     "upgrade")
       NEEDS_HELP=0
       pip_compile="${pip_compile} --upgrade"
-      ;;
+    ;;
     "help")
       NEEDS_HELP=1
-      ;;
+    ;;
     *)
       echo ""
       echo "ERROR: Parameter $1 not valid"
       echo ""
       NEEDS_HELP=1
-      ;;
-  esac
+    ;;
+  esac 
 
   if [[ "$NEEDS_HELP" == "1" ]] ; then
-    echo "This script generates pinned requirements.txt files from requirements[_*].in"
+    echo "This script generates requirements_all.txt from requirements[_*].in"
     echo ""
     echo "Usage: $0 [run|upgrade]"
     echo ""
@@ -75,16 +68,17 @@ main() {
     exit
   fi
 
+  cp -vf ${requirements_txt} "${_tmp}"
   cd "${_tmp}"
+
   generate_requirements
 
   echo "Changing $base_dir to requirements"
-  for file in requirements*.txt; do
-    sed "s:$base_dir:requirements:" "$file" > "$requirements_dir/$file"
-  done
+  cat requirements_all.txt | sed "s:$base_dir:requirements:" > "${requirements_txt}"
 
   _cleanup
 }
 
 # set EVAL=1 in case you want to source this script
 test "${EVAL:-0}" -eq "1" || main "${1:-}"
+
