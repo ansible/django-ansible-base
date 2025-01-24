f8fb4ea02fcc29e14a413a631dd5cccfdbdee3c2
Zack Kayyali <zkayyali812@gmail.com>
2025-01-24 15:20:44 -0500

    rename endpoint

--- ansible_base/feature_flags/urls.py
+++ ansible_base/feature_flags/urls.py
@@ -6,7 +6,7 @@ from ansible_base.feature_flags.apps import FeatureFlagsConfig
 app_name = FeatureFlagsConfig.label
 
 api_version_urls = [
-    path('feature-flags/definition', views.FeatureFlagsListView.as_view(), name='featureflags-list'),
+    path('feature_flags_definition', views.FeatureFlagsListView.as_view(), name='featureflags-list'),
 ]
 api_urls = []
 root_urls = []
