204c8128f6651f8efbd392078c3c69105c0e6b39
Bryan Havenstein <bhavenst@redhat.com>
2025-06-16 13:20:37 -0600

    Use extension format

--- ansible_base/feature_flags/views.py
+++ ansible_base/feature_flags/views.py
@@ -1,4 +1,5 @@
 from django.utils.translation import gettext_lazy as _
+from drf_spectacular.extensions import OpenApiViewExtension
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import OpenApiExample, extend_schema
 from rest_framework.response import Response
@@ -9,7 +10,6 @@ from ansible_base.lib.utils.views.ansible_base import AnsibleBaseView
 from .utils import get_django_flags
 
 
-@extend_schema(request=None, responses=OpenApiTypes.OBJECT, examples=[OpenApiExample(name="featureflags", value={"FLAG1": True, "FLAG2": False})])
 class FeatureFlagsStateListView(AnsibleBaseView):
     """
     A view class for displaying feature flags
@@ -26,3 +26,16 @@ class FeatureFlagsStateListView(AnsibleBaseView):
 
     def get_queryset(self):
         return get_django_flags()
+
+
+# Apply documentation definition for openapi (loaded if needed)
+class FeatureFlagsStateListViewAddDocs(OpenApiViewExtension):
+    target_class = FeatureFlagsStateListView
+
+    def view_replacement(self):
+        class FeatureFlagsStateListDocumentedView(self.target_class):
+            @extend_schema(request=None, responses=OpenApiTypes.OBJECT, examples=[OpenApiExample(name="featureflags", value={"FLAG1": True, "FLAG2": False})])
+            def get(self, request, format=None):
+                return super.get(request, format=None)
+
+        return FeatureFlagsStateListDocumentedView
