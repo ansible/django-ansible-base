60d4dedfc3fbc0c5eac8284b4362c04a4b78d647
Pablo Hess <phess@users.noreply.github.com>
2025-07-01 09:54:47 -0300

    Disable LDAP referrals by default

--- ansible_base/authentication/authenticator_plugins/ldap.py
+++ ansible_base/authentication/authenticator_plugins/ldap.py
@@ -309,7 +309,7 @@ class LDAPConfiguration(BaseAuthenticatorConfiguration):
             'https://www.python-ldap.org/doc/html/ldap.html#options for '
             'possible options and values that can be set.'
         ),
-        default={},
+        default={'OPT_REFERRALS': 0},
         allow_null=False,
         required=False,
         ui_field_label=_('LDAP Connection Options'),
