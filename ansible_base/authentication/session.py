f1ade289e549d00f5a4ab3836945a010069846cc
Brennan Paciorek <bpaciore@redhat.com>
2025-07-08 10:58:15 -0400

    fix comments, move setings patch to decorator

--- ansible_base/authentication/session.py
+++ ansible_base/authentication/session.py
@@ -1,31 +1,23 @@
-from django.conf import settings
 from rest_framework import authentication
 
-from ansible_base.lib.utils.settings import get_setting
+from ansible_base.lib.utils.settings import replace_trusted_origins
 
 
 class SessionAuthentication(authentication.SessionAuthentication):
     """
     This class allows us to fail with a 401 if the user is not authenticated.
 
-    Uses AnsibleBaseCsrfViewMiddleware for CSRF checking instead of Django's
-    default CsrfViewMiddleware, allowing CSRF_TRUSTED_ORIGINS to be read
-    dynamically using get_setting.
+    Allows CSRF_TRUSTED_ORIGINS to be read dynamically using get_setting.
+    Reverting the value of CSRF_TRUSTED_ORIGINS afterwards.
     """
 
     def authenticate_header(self, request):
         return "Session"
 
+    @replace_trusted_origins
     def enforce_csrf(self, request):
         """
         Enforce CSRF validation for session based authentication using
         AnsibleBaseCsrfViewMiddleware instead of Django's CsrfViewMiddleware.
         """
-        csrf_trusted_origins = settings.CSRF_TRUSTED_ORIGINS
-        try:
-            # Temporarily patch the setting
-            settings.CSRF_TRUSTED_ORIGINS = get_setting("CSRF_TRUSTED_ORIGINS", csrf_trusted_origins)
-            return super().enforce_csrf(request)
-        finally:
-            # Revert setting after this is done
-            settings.CSRF_TRUSTED_ORIGINS = csrf_trusted_origins
+        return super().enforce_csrf(request)
