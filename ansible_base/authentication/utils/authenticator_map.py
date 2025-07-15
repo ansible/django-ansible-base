6652cab375668e1668c00f7be76820d459937d16
John Westcott IV <john.westcott.iv@redhat.com>
2025-07-15 16:08:22 -0400

    Reducing congnative complexity

--- ansible_base/authentication/utils/authenticator_map.py
+++ ansible_base/authentication/utils/authenticator_map.py
@@ -1,6 +1,6 @@
 import logging
 import re
-from typing import Dict, Optional
+from typing import Any, Dict, Optional
 
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
@@ -33,15 +33,13 @@ def has_expansion(value: Optional[str]) -> bool:
 def check_expansion_syntax(value: Optional[str]) -> Optional[TranslatedString]:
     """
     Check a given field to see if it contains the proper syntax for {% for_attr_value(user_orgs) %}
-
-    Raises a ValidationError if its incorrect
     """
 
     if has_expansion(value) and not _EXPANSION_RE.search(value):
         return _("Expansion only supports the format {% for_attr_value(attribute) %}")
 
 
-def expand_syntax(attributes: dict, map: AuthenticatorMap) -> list[Dict[str, str]]:
+def expand_syntax(attributes: dict, auth_map: AuthenticatorMap) -> list[Dict[str, str]]:
     """
     Given attributes and a map, look for the fields that can be expanded and do the expansion
     If no fields require expansion, do nothing
@@ -51,7 +49,7 @@ def expand_syntax(attributes: dict, map: AuthenticatorMap) -> list[Dict[str, str
     """
     expanded_strings = {}
     for field in _EXPANSION_FIELDS:
-        field_value = getattr(map, field, None)
+        field_value = getattr(auth_map, field, None)
 
         if field_value is None:
             # If this particular field is None we can move on.
@@ -65,47 +63,25 @@ def expand_syntax(attributes: dict, map: AuthenticatorMap) -> list[Dict[str, str
 
         if has_expansion(field_value) and attrs_used == []:
             # This field had an invalid expansion so we can't expand anything, just return []
-            logger.info(f"Authenticator Map {map.name} on {map.authenticator.name} has a bad expansion in {field} unable to process map ({field_value})")
+            logger.info(
+                f"Authenticator Map {auth_map.name} on {auth_map.authenticator.name} has a bad expansion in {field} unable to process map ({field_value})"
+            )
             continue
 
         # Make sure that the attributes we got contain all the ones we want to expand
         if missing_attributes := set(attrs_used).difference(set(attributes.keys())):
             logger.info(
-                f"Authenticator Map {map.name} on {map.authenticator.name} tried to expand attribute(s) "
+                f"Authenticator Map {auth_map.name} on {auth_map.authenticator.name} tried to expand attribute(s) "
                 f"{', '.join(missing_attributes)} but they were not in the users attributes!"
             )
             continue
 
         # Normalize and check the attribute values we are going to use
-        normalized_attributes = {}
-        had_issues = False
-        for attr_name in attrs_used:
-            # Make sure the attribute is in the list of attributes we have.
-            normalized_attributes[attr_name] = attributes.get(attr_name, [])
-
-            # Make this a list if its not already a list. We can't do this as a typecast because list(dict) returns a list of keys
-            if type(normalized_attributes[attr_name]) is not list:
-                normalized_attributes[attr_name] = [normalized_attributes[attr_name]]
-
-            if len(normalized_attributes) == 0:
-                logger.info(
-                    f"Authenticator Map {map.name} on {map.authenticator.name} tried to expand attribute {attr_name} "
-                    f"but there were not values in that attribute, ignoring"
-                )
-                had_issues = True
-
-            # If any of the values are not a string then we can't expand it
-            for value in normalized_attributes[attr_name]:
-                if type(value) is not str:
-                    logger.info(
-                        f"Authenticator Map {map.name} on {map.authenticator.name} tried to expand attribute {attr_name} "
-                        f"but that was not a list of string, instead got {normalized_attributes[attr_name]}, ignoring"
-                    )
-                    had_issues = True
-                    break
-
-        # If we had issues normalizing the data we can just move on
-        if had_issues:
+        try:
+            normalized_attributes = _normalize_attributes(auth_map, attrs_used, attributes)
+        except ValueError:
+            # If we had issues normalizing the data we can just move on because if can't replace
+            # even one of the items in the string there is no point in trying at all
             continue
 
         # Create an entry in our expanded_string dictionary with the initial value of the field
@@ -127,20 +103,79 @@ def expand_syntax(attributes: dict, map: AuthenticatorMap) -> list[Dict[str, str
         #         ['Organization b - Department Z'
         #         ['Organization b - Department Y'
         #     ]
-        for attr_name in attrs_used:
-            # Make a list of new strings.
-            new_strings = []
-
-            for value in normalized_attributes[attr_name]:
-                for string in expanded_strings[field]:
-                    new_strings.append(_EXPANSION_RE.sub(value, string, 1))
-
-            # Set these string back into expanded_strings which will look like:
-            #    expanded_string: { 'role': [...], 'organization': [...], 'team': [...]}
-            expanded_strings[field] = new_strings
+        expanded_strings[field] = _expand_strings(attrs_used, normalized_attributes, expanded_strings[field])
 
     # Return the cartesian product of the expanded strings
     return dict_cartesian_product(expanded_strings)
+
+
+def _expand_strings(attrs_used: list[str], normalized_attributes: dict[str, list[str]], expanded_strings: list[str]) -> list[str]:
+    for attr_name in attrs_used:
+        # Make a list of new strings.
+        new_strings = []
+
+        for value in normalized_attributes[attr_name]:
+            for string in expanded_strings:
+                new_strings.append(_EXPANSION_RE.sub(value, string, 1))
+
+        expanded_strings = new_strings
+
+    return expanded_strings
+
+
+def _normalize_attributes(auth_map: AuthenticatorMap, attrs_used: list[str], attributes: dict) -> dict[str, list[str]]:
+    """
+    Normalize the attributes we are going to use
+
+    Given a list of attributes that are used (i.e. ["first_name", "last_name"]) create and return a dictionary of attributes lke:
+        {
+            "first_name": ["all", "values", "of", "first_name"],
+            "last_name": ["value"],
+        }
+
+    If the list contains non-strings we will raise errors
+
+    Raises ValueError if we got one or more issues when normalizing values
+    """
+    normalized_attributes = {}
+    errors = []
+    for attr_name in attrs_used:
+        attr_errors = []
+        # Make sure the attribute is in the list of attributes we have.
+        attr_value = _make_list(attributes.get(attr_name, []))
+
+        if len(attr_value) == 0:
+            attr_errors.append(
+                f"Authenticator Map {auth_map.name} on {auth_map.authenticator.name} tried to expand attribute {attr_name} "
+                f"but there were not values in that attribute, ignoring"
+            )
+
+        # If any of the values are not a string then we can't expand it
+        for value in attr_value:
+            if type(value) is not str:
+                attr_errors.append(
+                    f"Authenticator Map {auth_map.name} on {auth_map.authenticator.name} tried to expand attribute {attr_name} "
+                    f"but that was not a list of string, instead got {value}, ignoring"
+                )
+
+        if attr_errors == []:
+            normalized_attributes[attr_name] = attr_value
+        else:
+            # If we got errors for this item add them to the overall errors for logging
+            errors.extend(attr_errors)
+            for error in attr_errors:
+                logger.info(error)
+
+    if errors:
+        raise ValueError()
+
+    return normalized_attributes
+
+
+def _make_list(value: Any) -> list[Any]:
+    if type(value) is list:
+        return value
+    return [value]
 
 
 def _is_rbac_installed():
