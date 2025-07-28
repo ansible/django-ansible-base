0fda7a566f2c7e970357d1bd65d475ee2ecce9f4
Doston Toirov <doston.toirov2@gmail.com>
2025-07-28 13:39:33 -0400

    update fields to be read only

--- ansible_base/authentication/serializers/ui_auth.py
+++ ansible_base/authentication/serializers/ui_auth.py
@@ -4,24 +4,24 @@ from rest_framework import serializers
 class PasswordAuthenticatorSerializer(serializers.Serializer):
     """Serializer for password authenticator items in UI auth response."""
 
-    name = serializers.CharField()
+    name = serializers.CharField(read_only=True)
 
 
 class SSOAuthenticatorSerializer(serializers.Serializer):
     """Serializer for SSO authenticator items in UI auth response."""
 
-    name = serializers.CharField()
-    login_url = serializers.URLField()
-    type = serializers.CharField()
+    name = serializers.CharField(read_only=True)
+    login_url = serializers.URLField(read_only=True)
+    type = serializers.CharField(read_only=True)
 
 
 class UIAuthResponseSerializer(serializers.Serializer):
     """Serializer for UI authentication configuration response."""
 
-    passwords = PasswordAuthenticatorSerializer(many=True)
-    ssos = SSOAuthenticatorSerializer(many=True)
-    show_login_form = serializers.BooleanField()
-    login_redirect_override = serializers.CharField(allow_blank=True)
-    custom_login_info = serializers.CharField(allow_blank=True)
-    custom_logo = serializers.CharField(allow_blank=True)
-    managed_cloud_install = serializers.BooleanField()
+    passwords = PasswordAuthenticatorSerializer(many=True, read_only=True)
+    ssos = SSOAuthenticatorSerializer(many=True, read_only=True)
+    show_login_form = serializers.BooleanField(read_only=True)
+    login_redirect_override = serializers.CharField(allow_blank=True, read_only=True)
+    custom_login_info = serializers.CharField(allow_blank=True, read_only=True)
+    custom_logo = serializers.CharField(allow_blank=True, read_only=True)
+    managed_cloud_install = serializers.BooleanField(read_only=True)
