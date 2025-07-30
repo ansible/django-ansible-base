d2e5fcf106864eea900ad2e6356a659518ebed6b
Bryan Havenstein <bhavenst@redhat.com>
2025-07-30 17:22:40 -0600

    Sonar issue

--- ansible_base/authentication/views/ui_auth.py
+++ ansible_base/authentication/views/ui_auth.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Union
 
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
@@ -39,6 +40,24 @@ class UIAuth(AnsibleBaseDjangoAppApiView):
             return self._get()
 
 
+def _validate_and_get_login_redirect_override() -> Union[str, None]:
+
+    try:
+        login_redirect_override = get_setting('LOGIN_REDIRECT_OVERRIDE', '')
+        # ignore validation if login_redirect_override is None or empty string
+        if login_redirect_override is not None and login_redirect_override != '':
+            validate_url(url=login_redirect_override, allow_plain_hostname=True)
+            return login_redirect_override
+    except ValidationError:
+        # login_redirect_override can also be an absolute path
+        try:
+            validate_absolute_path(path=login_redirect_override)
+            return login_redirect_override
+        except ValidationError:
+            logger.error('LOGIN_REDIRECT_OVERRIDE was set but was not a valid URL or absolute path, ignoring')
+    return None
+
+
 def generate_ui_auth_data():
     authenticators = Authenticator.objects.filter(enabled=True)
     response = {
@@ -68,19 +87,9 @@ def generate_ui_auth_data():
         else:
             logger.error(f"Don't know how to handle authenticator of type {authenticator.type}")
 
-    try:
-        login_redirect_override = get_setting('LOGIN_REDIRECT_OVERRIDE', '')
-        # ignore validation if login_redirect_override is None or empty string
-        if login_redirect_override is not None and login_redirect_override != '':
-            validate_url(url=login_redirect_override, allow_plain_hostname=True)
-            response['login_redirect_override'] = login_redirect_override
-    except ValidationError:
-        # login_redirect_override can also be an absolute path
-        try:
-            validate_absolute_path(path=login_redirect_override)
-            response['login_redirect_override'] = login_redirect_override
-        except ValidationError:
-            logger.error('LOGIN_REDIRECT_OVERRIDE was set but was not a valid URL or absolute path, ignoring')
+    login_redirect_override = _validate_and_get_login_redirect_override()
+    if login_redirect_override:
+        response['login_redirect_override'] = login_redirect_override
 
     custom_login_info = get_setting('custom_login_info', '')
     if isinstance(custom_login_info, str):
