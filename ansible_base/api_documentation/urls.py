1bd24d346fd136a9e0a18834e223c3bade84e41f
Hao Liu <44379968+TheRealHaoLiu@users.noreply.github.com>
2025-12-10 14:30:50 -0500

    Add API docs index endpoint and update docs URLs
    Introduces DocsRootView to provide an index of API documentation endpoints at /docs/. Updates URL patterns to include /docs/swagger/, /docs/redoc/, and /docs/schema/. Documentation updated to reflect new endpoints.

--- ansible_base/api_documentation/urls.py
+++ ansible_base/api_documentation/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 from ansible_base.api_documentation.apps import ApiDocumentationConfig
+from ansible_base.api_documentation.views import DocsRootView
 
 app_name = ApiDocumentationConfig.label
 api_version_urls = [
-    path('docs/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('docs/', DocsRootView.as_view(), name='docs-root'),
+    path('docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('docs/schema/', SpectacularAPIView.as_view(), name='schema'),
 ]
