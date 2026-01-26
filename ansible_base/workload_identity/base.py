9806d651c5f6be81d331b355e68700cdb65e7d67
melissalkelly <mekelly@redhat.com>
2026-01-26 09:02:22 -0500

    Add abstractmethod, update docstrings
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- ansible_base/workload_identity/base.py
+++ ansible_base/workload_identity/base.py
@@ -2,6 +2,8 @@
 Base class for OIDC workload identity scopes.
 """
 
+from abc import abstractmethod
+
 
 class BaseWorkloadIdentityScope:
     """
@@ -11,14 +13,25 @@ class BaseWorkloadIdentityScope:
     name = ""
     description = ""
 
+    @abstractmethod
     def list_claims(self) -> list[str]:
         """
         Return a list of all claim names defined in this scope.
+
+        :return: A list of claim name strings.
+        :rtype: list[str]
         """
         raise NotImplementedError("Subclasses must implement list_claims()")
 
-    def populate_claims(self, context: dict) -> dict:
+    @abstractmethod
+    def populate_claims(self, workload_data: dict) -> dict:
         """
-        Populate claims from the provided context.
+        Populate claims based on the provided workload data.
+
+        :param workload_data: The workload_data dictionary used to populate claim values.
+                        This workload_data identifies the AAP workload that is requesting a scope.
+        :type workload_data: dict
+        :return: A dictionary mapping claim names to their corresponding values generated from the workload_data.
+        :rtype: dict
         """
         raise NotImplementedError("Subclasses will implement populate_claims() in future iterations")
