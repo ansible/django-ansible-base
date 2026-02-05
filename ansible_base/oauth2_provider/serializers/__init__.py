ad3fb7a60ffc49c97fc0d53f6a1ca615eb4fb1f6
Daniel Finca <dfinca@redhat.com>
2026-02-05 12:22:29 +0100

    Move serializers to lib folder under ansible_base

--- ansible_base/oauth2_provider/serializers/__init__.py
+++ ansible_base/oauth2_provider/serializers/__init__.py
@@ -1,3 +1,2 @@
 from .application import OAuth2ApplicationSerializer  # noqa: F401
 from .token import OAuth2TokenSerializer  # noqa: F401
-from .workload_identity_tokens import WorkloadIdentityTokenRequestSerializer, WorkloadIdentityTokenResponseSerializer  # noqa: F401
