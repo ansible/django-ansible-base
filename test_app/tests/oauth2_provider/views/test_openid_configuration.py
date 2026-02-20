f0c0df0bd375c7b51eceb7cb8f6a024b90b19777
Dan Leehr <dleehr@redhat.com>
2026-02-20 11:49:47 -0500

    Reorder settings and feature flag

--- test_app/tests/oauth2_provider/views/test_openid_configuration.py
+++ test_app/tests/oauth2_provider/views/test_openid_configuration.py
@@ -11,8 +11,8 @@ def test_oauth2_provider_openid_configuration_valid_issuer_url(client):
     As an anonymous user, accessing /o/.well-known/openid-configuration/ should include
     an issuer URL that ends with /o
     """
-    with override_settings(OAUTH2_PROVIDER={'OIDC_ENABLED': True}):
-        with feature_flag_enabled('FEATURE_OIDC_WORKLOAD_IDENTITY_ENABLED'):
+    with feature_flag_enabled('FEATURE_OIDC_WORKLOAD_IDENTITY_ENABLED'):
+        with override_settings(OAUTH2_PROVIDER={'OIDC_ENABLED': True}):
             url = get_relative_url("oauth2_provider:oidc-connect-discovery-info")
             response_json = client.get(url).json()
             assert response_json['issuer'].endswith(
