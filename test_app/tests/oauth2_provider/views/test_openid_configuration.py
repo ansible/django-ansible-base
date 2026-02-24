cc55a6769d44659769c1cd5a8f7557dc3a24ec19
Dan Leehr <dleehr@redhat.com>
2026-02-24 10:33:13 -0500

    Add negative test to show oidc discovery endpoint is 404 when feature disabled

--- test_app/tests/oauth2_provider/views/test_openid_configuration.py
+++ test_app/tests/oauth2_provider/views/test_openid_configuration.py
@@ -1,7 +1,7 @@
 import pytest
 from django.test import override_settings
 
-from ansible_base.lib.testing.util import feature_flag_enabled
+from ansible_base.lib.testing.util import feature_flag_disabled, feature_flag_enabled
 from ansible_base.lib.utils.response import get_relative_url
 
 
@@ -20,3 +20,15 @@ def test_oauth2_provider_openid_configuration_valid_issuer_url(client):
             assert response_json['issuer'].endswith(
                 '/o'
             ), "issuer in discovery metadata is expected to end with /o and match the authorization root view, otherwise discovery will fail!"
+
+
+@pytest.mark.django_db
+def test_oauth2_provider_openid_configuration_404_without_feature_flag(client):
+    """
+    As an anonymous user, accessing /o/.well-known/openid-configuration/ should fail
+    with 404 if the feature is not enabled
+    """
+    with feature_flag_disabled('FEATURE_OIDC_WORKLOAD_IDENTITY_ENABLED'):
+        url = get_relative_url("oauth2_provider:oidc-connect-discovery-info")
+        response = client.get(url)
+        assert response.status_code == 404, f"Expected 404, got {response.status_code}"
