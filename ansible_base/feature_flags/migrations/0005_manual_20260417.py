bbf5c14efd1ce3c1bdf57b8a2798ca026ab392a1
Ciaran Shiels <cas.ireland@Gmail.com>
2026-04-17 14:16:05 +0100

    Add FEATURE_DASHBOARD_COLLECTION_ENABLED flag and migration
    Introduced a new feature flag, FEATURE_DASHBOARD_COLLECTION_ENABLED, to enable data collection for the automation-reports dashboard integration. This flag includes a description, visibility settings, and is set to run-time toggle type. Additionally, a migration file has been created to support this new feature flag in the database schema.

---
+++ ansible_base/feature_flags/migrations/0005_manual_20260417.py
@@ -0,0 +1,17 @@
+###
+# Addition of FEATURE_DASHBOARD_COLLECTION_ENABLED
+###
+
+# FileHash: 3b05aadcaf50652c8bd5311656dcbce325cc6510606e5ba84fd8494d386fbad5
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dab_feature_flags', '0004_remove_dispatcherd_feature_flag'),
+    ]
+
+    operations = [
+    ]
