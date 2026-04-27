11670785576330c7ca127b806ae68b85c8c70930
Ciaran Shiels <cas.ireland@Gmail.com>
2026-04-27 15:29:02 +0100

    Update feature flag description and migration file hash for FEATURE_DASHBOARD_COLLECTION_ENABLED
    - Revised the description in feature_flags.yaml to clarify that job data is stored locally in the platform database.
    - Updated the file hash in the migration script to reflect changes made to the feature flag.

--- ansible_base/feature_flags/migrations/0005_manual_20260417.py
+++ ansible_base/feature_flags/migrations/0005_manual_20260417.py
@@ -2,7 +2,7 @@
 # Addition of FEATURE_DASHBOARD_COLLECTION_ENABLED
 ###
 
-# FileHash: 3b05aadcaf50652c8bd5311656dcbce325cc6510606e5ba84fd8494d386fbad5
+# FileHash: 2f5c26fd5d1e8a3e93ee14f76b3dd7fcebff316ab07fc27b6d3e8f22c2740e01
 
 from django.db import migrations
 
