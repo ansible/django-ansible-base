f3d88a58a9f109fa2b23463cadeaae4b027419c3
Alex <alex.swansboro@gmail.com>
2026-07-02 14:30:36 -0400

    Revert "[AAP-80669] ServiceCursorPagination used ordering  (#1042)"
    This reverts commit a6d977229315bab12fa50ed3fcec6e511f861a95.

--- ansible_base/rbac/api/service_cursor_paginator.py
+++ ansible_base/rbac/api/service_cursor_paginator.py
@@ -3,6 +3,6 @@ from rest_framework.pagination import CursorPagination
 
 class ServiceCursorPagination(CursorPagination):
     page_size = 50
-    ordering = 'id'
+    ordering = '-id'  # Order by 'id' to ensure consistent pagination results
     page_size_query_param = 'page_size'
     max_page_size = 1000
