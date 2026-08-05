47d1be6417e32d6f58dc61f0650745c45a18ffde
Maeve Hoffer <dhoffer@redhat.com>
2026-08-05 09:25:57 -0700

    add max length validation and resolve import issue

--- ansible_base/lib/validation.py
+++ ansible_base/lib/validation.py
@@ -3,10 +3,11 @@ from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
 
 resource_name_validator = RegexValidator(
-    regex=r'^[\w][\w .@-]*\Z',
+    regex=r'^[\w][\w .@-]{0,511}\Z',
     message=_(
         "Enter a valid resource name. Must start with a letter, digit, or underscore."
         " After the first character, spaces, dots, @, and hyphens are also allowed."
+        " Maximum length is 512 characters."
         " Special characters like <, >, &, |, ;, ', \", $, and path separators are not permitted."
     ),
     code="invalid_resource_name",
